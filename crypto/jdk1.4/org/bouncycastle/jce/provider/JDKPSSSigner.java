f30db64e6c6dfd1473fce78c16da2eb77228e7ee
bouncy <bouncy>
2005-10-23 22:11:35 +0000

    minor formatting changes

--- crypto/jdk1.4/org/bouncycastle/jce/provider/JDKPSSSigner.java
+++ crypto/jdk1.4/org/bouncycastle/jce/provider/JDKPSSSigner.java
@@ -49,7 +49,7 @@ public class JDKPSSSigner
         PublicKey   publicKey)
         throws InvalidKeyException
     {
-        if ( !(publicKey instanceof RSAPublicKey) )
+        if (!(publicKey instanceof RSAPublicKey))
         {
             throw new InvalidKeyException("Supplied key is not a RSAPublicKey instance");
         }
@@ -64,7 +64,7 @@ public class JDKPSSSigner
         SecureRandom    random)
         throws InvalidKeyException
     {
-        if ( !(privateKey instanceof RSAPrivateKey) )
+        if (!(privateKey instanceof RSAPrivateKey))
         {
             throw new InvalidKeyException("Supplied key is not a RSAPrivateKey instance");
         }
@@ -77,7 +77,7 @@ public class JDKPSSSigner
         PrivateKey  privateKey)
         throws InvalidKeyException
     {
-        if ( !(privateKey instanceof RSAPrivateKey) )
+        if (!(privateKey instanceof RSAPrivateKey))
         {
             throw new InvalidKeyException("Supplied key is not a RSAPrivateKey instance");
         }
@@ -105,7 +105,8 @@ public class JDKPSSSigner
     protected byte[] engineSign()
         throws SignatureException
     {
-        try {
+        try
+        {
             return pss.generateSignature();
         }
         catch (CryptoException e)
@@ -125,7 +126,7 @@ public class JDKPSSSigner
         AlgorithmParameterSpec params)
         throws InvalidParameterException
     {
-        if ( params instanceof PSSParameterSpec )
+        if (params instanceof PSSParameterSpec)
         {
             saltLength = ((PSSParameterSpec)params).getSaltLength();
         }
