c01f3c1f2f1f09ff5ab0ec57e54b6eb2d9a63b0c
bouncy <bouncy>
2005-10-23 07:46:40 +0000

    rework of test classes

--- crypto/src/org/bouncycastle/util/test/SimpleTestResult.java
+++ crypto/src/org/bouncycastle/util/test/SimpleTestResult.java
@@ -6,7 +6,7 @@ public class SimpleTestResult implements TestResult
 
     private boolean             success;
     private String              message;
-    private Exception           exception;
+    private Throwable           exception;
 
     public SimpleTestResult(boolean success, String message)
     {
@@ -14,23 +14,44 @@ public class SimpleTestResult implements TestResult
         this.message = message;
     }
 
-    public SimpleTestResult(boolean success, String message, Exception exception)
+    public SimpleTestResult(boolean success, String message, Throwable exception)
     {
         this.success = success;
         this.message = message;
         this.exception = exception;
     }
 
-    public static TestResult successful(String message)
+    public static TestResult successful(
+        Test test, 
+        String message)
     {
-        return new SimpleTestResult(true, message);
+        return new SimpleTestResult(true, test.getName() + ": " + message);
     }
 
-    public static TestResult failed(String message)
+    public static TestResult failed(
+        Test test, 
+        String message)
     {
-        return new SimpleTestResult(false, message);
+        return new SimpleTestResult(false, test.getName() + ": " + message);
     }
-
+    
+    public static TestResult failed(
+        Test test, 
+        String message, 
+        Throwable t)
+    {
+        return new SimpleTestResult(false, test.getName() + ": " + message, t);
+    }
+    
+    public static TestResult failed(
+        Test test, 
+        String message, 
+        Object expected, 
+        Object found)
+    {
+        return failed(test, message + SEPARATOR + "Expected: " + expected + SEPARATOR + "Found   : " + found);
+    }
+    
     public static String failedMessage(String algorithm, String testName, String expected,
             String actual)
     {
@@ -52,7 +73,7 @@ public class SimpleTestResult implements TestResult
         return message;
     }
 
-    public Exception getException()
+    public Throwable getException()
     {
         return exception;
     }
