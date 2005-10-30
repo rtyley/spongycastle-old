e1de71136d063bfdaf67844bdbf76a67161e47e2
bouncy <bouncy>
2005-10-30 10:12:43 +0000

    test for compressed signed/encrypted signed

--- crypto/test/src/org/bouncycastle/mail/smime/test/AllTests.java
+++ crypto/test/src/org/bouncycastle/mail/smime/test/AllTests.java
@@ -1,14 +1,19 @@
 package org.bouncycastle.mail.smime.test;
 
+import java.security.Security;
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class AllTests
 {
     public static void main (String[] args)
         throws Exception
     {
+        Security.addProvider(new BouncyCastleProvider());
+
         junit.textui.TestRunner.run (suite());
     }
     
@@ -20,6 +25,7 @@ public class AllTests
         suite.addTest(org.bouncycastle.mail.smime.test.SMIMESignedTest.suite());
         suite.addTest(org.bouncycastle.mail.smime.test.SMIMEEnvelopedTest.suite());
         suite.addTest(org.bouncycastle.mail.smime.test.SMIMECompressedTest.suite());
+        suite.addTest(org.bouncycastle.mail.smime.test.SMIMEMiscTest.suite());
         return suite;
     }
 }
