7a1b2699fdfa155c510c73220ef3104417c9a6d6
bouncy <bouncy>
2005-11-23 12:28:14 +0000

    added getDEREncoded method

--- crypto/src/org/bouncycastle/asn1/ASN1Encodable.java
+++ crypto/src/org/bouncycastle/asn1/ASN1Encodable.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 public abstract class ASN1Encodable
     implements DEREncodable
 {
+    public static final String DER = "DER";
+    public static final String BER = "BER";
+    
     public byte[] getEncoded() 
         throws IOException
     {
@@ -15,6 +18,40 @@ public abstract class ASN1Encodable
         aOut.writeObject(this);
         
         return bOut.toByteArray();
+    }
+    
+    public byte[] getEncoded(
+        String encoding) 
+        throws IOException
+    {
+        if (encoding.equals(DER))
+        {
+            ByteArrayOutputStream   bOut = new ByteArrayOutputStream();
+            DEROutputStream         dOut = new DEROutputStream(bOut);
+            
+            dOut.writeObject(this);
+            
+            return bOut.toByteArray();
+        }
+        
+        return this.getEncoded();
+    }
+    
+    /**
+     * Return the DER encoding of the object, null if the DER encoding can not be made.
+     * 
+     * @return a DER byte array, null otherwise.
+     */
+    public byte[] getDEREncoded()
+    {
+        try
+        {
+            return this.getEncoded(DER);
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
     }
     
     public int hashCode()
