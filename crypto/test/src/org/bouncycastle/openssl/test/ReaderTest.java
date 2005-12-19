02885c3b8f381ce221ecbb7e446db129cb5fadb0
bouncy <bouncy>
2005-12-19 10:18:28 +0000

    additional encoding tests

--- crypto/test/src/org/bouncycastle/openssl/test/ReaderTest.java
+++ crypto/test/src/org/bouncycastle/openssl/test/ReaderTest.java
@@ -1,22 +1,33 @@
 package org.bouncycastle.openssl.test;
 
-import java.io.*;
-import java.security.*;
-import java.security.interfaces.*;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 import org.bouncycastle.asn1.cms.CMSObjectIdentifiers;
 import org.bouncycastle.asn1.cms.ContentInfo;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openssl.*;
-import org.bouncycastle.util.test.SimpleTestResult;
-import org.bouncycastle.util.test.Test;
-import org.bouncycastle.util.test.TestResult;
+import org.bouncycastle.openssl.PEMReader;
+import org.bouncycastle.openssl.PEMWriter;
+import org.bouncycastle.openssl.PasswordFinder;
+import org.bouncycastle.util.test.SimpleTest;
 
 /**
  * basic class for reading test.pem - the password is "secret"
  */
 public class ReaderTest
-    implements Test
+    extends SimpleTest
 {
     private static class Password
         implements PasswordFinder
@@ -40,80 +51,122 @@ public class ReaderTest
         return "PEMReaderTest";
     }
 
-    public TestResult perform()
+    public void performTest()
+        throws Exception
     {
         Reader          fRd =new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("test.pem")));
         PasswordFinder  pGet = new Password("secret".toCharArray());
         PEMReader       pemRd = new PEMReader(fRd, pGet);
         Object          o;
 
-        try
+        while ((o = pemRd.readObject()) != null)
         {
-            while ((o = pemRd.readObject()) != null)
+            if (o instanceof KeyPair)
             {
-                if (o instanceof KeyPair)
-                {
-                    KeyPair     pair = (KeyPair)o;
-            
-                    //System.out.println(pair.getPublic());
-                    //System.out.println(pair.getPrivate());
-                }
-                else
-                {
-                    //System.out.println(o.toString());
-                }
+                KeyPair     pair = (KeyPair)o;
+        
+                //System.out.println(pair.getPublic());
+                //System.out.println(pair.getPrivate());
             }
-            
-            //
-            // pkcs 7 data
-            //
-            fRd =new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("pkcs7.pem")));
-            pemRd = new PEMReader(fRd);
-            
-            ContentInfo d = (ContentInfo)pemRd.readObject();    
-                
-            if (!d.getContentType().equals(CMSObjectIdentifiers.envelopedData))
+            else
             {
-                return new SimpleTestResult(false, getName() + ": failed envelopedData check");
+                //System.out.println(o.toString());
             }
-            
-            //
-            // writer/reader test
-            //
-            ByteArrayOutputStream bOut = new ByteArrayOutputStream();
-            PEMWriter             pWrt = new PEMWriter(new OutputStreamWriter(bOut));
-            KeyPairGenerator      kpGen = KeyPairGenerator.getInstance("RSA", "BC");
-            KeyPair               pair = kpGen.generateKeyPair();
-            
-            pWrt.writeObject(pair.getPublic());
-            
-            pWrt.close();
-            
-            pemRd = new PEMReader(new InputStreamReader(new ByteArrayInputStream(bOut.toByteArray())));
-            
-            RSAPublicKey k = (RSAPublicKey)pemRd.readObject();
-            if (!k.equals(pair.getPublic()))
-            {
-                return new SimpleTestResult(false, getName() + ": Failed key read");
-            }
-        }
-        catch (Exception e)
-        {
-            return new SimpleTestResult(false, getName() + ": exception - " + e.toString(), e);
         }
         
-        return new SimpleTestResult(true, getName() + ": Okay");
+        //
+        // pkcs 7 data
+        //
+        fRd = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("pkcs7.pem")));
+        pemRd = new PEMReader(fRd);
+        
+        ContentInfo d = (ContentInfo)pemRd.readObject();    
+            
+        if (!d.getContentType().equals(CMSObjectIdentifiers.envelopedData))
+        {
+            fail("failed envelopedData check");
+        }
+        
+        //
+        // writer/reader test
+        //
+        KeyPairGenerator      kpGen = KeyPairGenerator.getInstance("RSA", "BC");
+        KeyPair               pair = kpGen.generateKeyPair();
+        
+        keyPairTest("RSA", pair);
+        
+        kpGen = KeyPairGenerator.getInstance("DSA", "BC");
+        kpGen.initialize(512, new SecureRandom());
+        pair = kpGen.generateKeyPair();
+        
+        keyPairTest("DSA", pair);
+        
+        //
+        // PKCS7
+        //
+        ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+        PEMWriter             pWrt = new PEMWriter(new OutputStreamWriter(bOut));
+        
+        pWrt.writeObject(d);
+        
+        pWrt.close();
+        
+        pemRd = new PEMReader(new InputStreamReader(new ByteArrayInputStream(bOut.toByteArray())));
+        d = (ContentInfo)pemRd.readObject();    
+        
+        if (!d.getContentType().equals(CMSObjectIdentifiers.envelopedData))
+        {
+            fail("failed envelopedData recode check");
+        }
+    }
+
+    private void keyPairTest(
+        String   name,
+        KeyPair pair) 
+        throws IOException
+    {
+        PEMReader pemRd;
+        ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+        PEMWriter             pWrt = new PEMWriter(new OutputStreamWriter(bOut));
+        
+        pWrt.writeObject(pair.getPublic());
+        
+        pWrt.close();
+
+        pemRd = new PEMReader(new InputStreamReader(new ByteArrayInputStream(bOut.toByteArray())));
+        
+        PublicKey k = (PublicKey)pemRd.readObject();
+        if (!k.equals(pair.getPublic()))
+        {
+            fail("Failed public key read: " + name);
+        }
+        
+        bOut = new ByteArrayOutputStream();
+        pWrt = new PEMWriter(new OutputStreamWriter(bOut));
+        
+        pWrt.writeObject(pair.getPrivate());
+        
+        pWrt.close();
+        
+        pemRd = new PEMReader(new InputStreamReader(new ByteArrayInputStream(bOut.toByteArray())));
+        
+        KeyPair kPair = (KeyPair)pemRd.readObject();
+        if (!kPair.getPrivate().equals(pair.getPrivate()))
+        {
+            fail("Failed private key read: " + name);
+        }
+        
+        if (!kPair.getPublic().equals(pair.getPublic()))
+        {
+            fail("Failed private key public read: " + name);
+        }
     }
     
     public static void main(
-        String[]  args)
-        throws Exception
+        String[]    args)
     {
         Security.addProvider(new BouncyCastleProvider());
 
-        Test            test = new ReaderTest();
-        TestResult      result = test.perform();
-
-        System.out.println(result.toString());
+        runTest(new ReaderTest());
     }
 }
