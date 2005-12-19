13452e6ba3ccd66ef3e5027db6e09923e255135a
bouncy <bouncy>
2005-12-19 10:19:54 +0000

    fixes to reflect changes in 1.4/1.5

--- crypto/jdk1.3/org/bouncycastle/x509/AttributeCertificateHolder.java
+++ crypto/jdk1.3/org/bouncycastle/x509/AttributeCertificateHolder.java
@@ -6,23 +6,22 @@ import org.bouncycastle.asn1.DERInteger;
 import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.asn1.x509.GeneralName;
 import org.bouncycastle.asn1.x509.GeneralNames;
+import org.bouncycastle.asn1.x509.Holder;
 import org.bouncycastle.asn1.x509.IssuerSerial;
-import org.bouncycastle.asn1.x509.X509Name;
 import org.bouncycastle.jce.PrincipalUtil;
 import org.bouncycastle.jce.X509Principal;
 
 import java.io.IOException;
 import java.math.BigInteger;
 
-import org.bouncycastle.jce.cert.CertSelector;
-
 import java.security.Principal;
+import org.bouncycastle.jce.cert.CertSelector;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateParsingException;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The Holder object.
@@ -38,16 +37,17 @@ import java.util.ArrayList;
  *                 -- for example, an executable
  *  }
  * </pre>
+ * This holder currently supports use of the baseCertificateID and the entityName.
  */
 public class AttributeCertificateHolder 
     implements CertSelector
 {
-    org.bouncycastle.asn1.x509.Holder   holder;
+    Holder   holder;
 
     AttributeCertificateHolder(
         ASN1Sequence seq)
     {
-        holder = org.bouncycastle.asn1.x509.Holder.getInstance(seq);
+        holder = Holder.getInstance(seq);
     }
 
     public AttributeCertificateHolder(
@@ -65,13 +65,18 @@ public class AttributeCertificateHolder
             throw new CertificateParsingException(e.getMessage());
         }
         
-        holder = new org.bouncycastle.asn1.x509.Holder(new IssuerSerial(new GeneralNames(new DERSequence(new GeneralName(new X509Principal(name)))), new DERInteger(cert.getSerialNumber())));
+        holder = new Holder(new IssuerSerial(generateGeneralNames(name), new DERInteger(cert.getSerialNumber())));
     }
     
     public AttributeCertificateHolder(
         X509Principal principal) 
     {        
-        holder = new org.bouncycastle.asn1.x509.Holder(new GeneralNames(new DERSequence(new GeneralName(principal))));
+        holder = new Holder(generateGeneralNames(principal));
+    }
+    
+    private GeneralNames generateGeneralNames(X509Principal principal)
+    {
+        return new GeneralNames(new DERSequence(new GeneralName(principal)));
     }
     
     private boolean matchesDN(X509Principal subject, GeneralNames targets)
@@ -101,49 +106,49 @@ public class AttributeCertificateHolder
     }
 
     private Object[] getNames(
-            GeneralName[] names)
-        {        
-            List   l = new ArrayList(names.length);
-            
-            for (int i = 0; i != names.length; i++)
+        GeneralName[] names)
+    {        
+        List        l = new ArrayList(names.length);
+        
+        for (int i = 0; i != names.length; i++)
+        {
+            if (names[i].getTagNo() == GeneralName.directoryName)
             {
-                if (names[i].getName() instanceof X509Name)
+                try
                 {
-                    try
-                    {
-                        l.add(new X509Principal(((X509Name)names[i].getName()).getEncoded()));
-                    }
-                    catch (IOException e)
-                    {
-                        throw new RuntimeException("badly formed Name object");
-                    }
+                    l.add(new X509Principal(((ASN1Encodable)names[i].getName()).getEncoded()));
+                }
+                catch (IOException e)
+                {
+                    throw new RuntimeException("badly formed Name object");
                 }
             }
-            
-            return l.toArray(new Object[l.size()]);
+        }
+
+        return l.toArray(new Object[l.size()]);
+    }
+    
+    private Principal[] getPrincipals(
+        GeneralNames    names)
+    {
+        Object[]    p = this.getNames(names.getNames());
+        List        l = new ArrayList();
+        
+        for (int i = 0; i != p.length; i++)
+        {
+            if (p[i] instanceof Principal)
+            {
+                l.add(p[i]);
+            }
         }
         
-        private Principal[] getPrincipals(
-            GeneralNames    names)
-        {
-            Object[]    p = this.getNames(names.getNames());
-            List        l = new ArrayList();
-            
-            for (int i = 0; i != p.length; i++)
-            {
-                if (p[i] instanceof Principal)
-                {
-                    l.add(p[i]);
-                }
-            }
-            
-            return (Principal[])l.toArray(new Principal[l.size()]);
-        }
+        return (Principal[])l.toArray(new Principal[l.size()]);
+    }
     
     /**
      * Return any principal objects inside the attribute certificate holder entity names field.
      * 
-     * @return an array of Principal objects (usually X509Principal), null if no entity names field is set.
+     * @return an array of Principal objects (usually X500Principal), null if no entity names field is set.
      */
     public Principal[] getEntityNames()
     {
