f77d8909807fc30b75eb9dcb66fe7861b2ae9e2f
bouncy <bouncy>
2006-02-06 10:07:20 +0000

    added convertMidTerms method

--- crypto/src/org/bouncycastle/jce/provider/ECUtil.java
+++ crypto/src/org/bouncycastle/jce/provider/ECUtil.java
@@ -18,6 +18,76 @@ import org.bouncycastle.jce.spec.ECParameterSpec;
  */
 public class ECUtil
 {
+    /**
+     * Returns a sorted array of middle terms of the reduction polynomial.
+     * @param k The unsorted array of middle terms of the reduction polynomial
+     * of length 1 or 3.
+     * @return the sorted array of middle terms of the reduction polynomial.
+     * This array always has length 3.
+     */
+    static int[] convertMidTerms(
+        int[] k)
+    {
+        int[] res = new int[3];
+        
+        if (k.length == 1)
+        {
+            res[0] = k[0];
+        }
+        else
+        {
+            if (k.length != 3)
+            {
+                throw new IllegalArgumentException("Only Trinomials and pentanomials supported");
+            }
+
+            if (k[0] < k[1] && k[0] < k[2])
+            {
+                res[0] = k[0];
+                if (k[1] < k[2])
+                {
+                    res[1] = k[1];
+                    res[2] = k[2];
+                }
+                else
+                {
+                    res[1] = k[2];
+                    res[2] = k[1];
+                }
+            }
+            else if (k[1] < k[2])
+            {
+                res[0] = k[1];
+                if (k[0] < k[2])
+                {
+                    res[1] = k[0];
+                    res[2] = k[2];
+                }
+                else
+                {
+                    res[1] = k[2];
+                    res[2] = k[0];
+                }
+            }
+            else
+            {
+                res[0] = k[2];
+                if (k[0] < k[1])
+                {
+                    res[1] = k[0];
+                    res[2] = k[1];
+                }
+                else
+                {
+                    res[1] = k[1];
+                    res[2] = k[0];
+                }
+            }
+        }
+
+        return res;
+    }
+
     static public AsymmetricKeyParameter generatePublicKeyParameter(
         PublicKey    key)
         throws InvalidKeyException
@@ -29,7 +99,7 @@ public class ECUtil
 
             return new ECPublicKeyParameters(
                             k.getQ(),
-                            new ECDomainParameters(s.getCurve(), s.getG(), s.getN()));
+                            new ECDomainParameters(s.getCurve(), s.getG(), s.getN(), s.getH(), s.getSeed()));
         }
 
         throw new InvalidKeyException("can't identify EC public key.");
@@ -46,7 +116,7 @@ public class ECUtil
 
             return new ECPrivateKeyParameters(
                             k.getD(),
-                            new ECDomainParameters(s.getCurve(), s.getG(), s.getN()));
+                            new ECDomainParameters(s.getCurve(), s.getG(), s.getN(), s.getH(), s.getSeed()));
         }
                         
         throw new InvalidKeyException("can't identify EC private key.");
