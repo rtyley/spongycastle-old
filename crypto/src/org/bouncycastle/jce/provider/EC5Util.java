b680f1a29c7d4c8931c581580867ed0b30bba0da
bouncy <bouncy>
2006-02-11 09:43:57 +0000

    general fixes for f2m to allow working with JDK 1.5 APIs

---
+++ crypto/src/org/bouncycastle/jce/provider/EC5Util.java
@@ -0,0 +1,54 @@
+package org.bouncycastle.jce.provider;
+
+import java.security.spec.ECFieldF2m;
+import java.security.spec.ECFieldFp;
+import java.security.spec.ECParameterSpec;
+import java.security.spec.ECPoint;
+import java.security.spec.EllipticCurve;
+
+import org.bouncycastle.math.ec.ECCurve;
+
+public class EC5Util
+{
+    static EllipticCurve convertCurve(
+        ECCurve curve, 
+        byte[]  seed)
+    {
+        if (curve instanceof ECCurve.Fp)
+        {
+            return new EllipticCurve(new ECFieldFp(((ECCurve.Fp)curve).getQ()), curve.getA().toBigInteger(), curve.getB().toBigInteger(), seed);
+        }
+        else
+        {
+            ECCurve.F2m curveF2m = (ECCurve.F2m)curve;
+            int ks[];
+            
+            if (curveF2m.isTrinomial())
+            {
+                ks = new int[] { curveF2m.getK1() };
+                
+                return new EllipticCurve(new ECFieldF2m(curveF2m.getM(), ks), curve.getA().toBigInteger(), curve.getB().toBigInteger(), seed);
+            }
+            else
+            {
+                ks = new int[] { curveF2m.getK1(), curveF2m.getK2(), curveF2m.getK3() };
+                
+                return new EllipticCurve(new ECFieldF2m(curveF2m.getM(), ks), curve.getA().toBigInteger(), curve.getB().toBigInteger(), seed);
+            } 
+        }
+    }
+    
+
+    static ECParameterSpec convertSpec(
+        EllipticCurve ellipticCurve,
+        org.bouncycastle.jce.spec.ECParameterSpec spec)
+    {
+        return new ECParameterSpec(
+                ellipticCurve,
+                new ECPoint(
+                        spec.getG().getX().toBigInteger(),
+                        spec.getG().getY().toBigInteger()),
+                        spec.getN(),
+                        spec.getH().intValue());
+    }
+}
