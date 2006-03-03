a82bb694aeb1d6f2d766041b451e0731fa8d063b
bouncy <bouncy>
2006-03-03 22:05:13 +0000

    fixed error in handling of DER constructed octet strings

--- crypto/src/org/bouncycastle/sasn1/DerOctetString.java
+++ crypto/src/org/bouncycastle/sasn1/DerOctetString.java
@@ -1,6 +1,5 @@
 package org.bouncycastle.sasn1;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 public class DerOctetString 
@@ -23,60 +22,6 @@ public class DerOctetString
         else
         {
             return this.getRawContentStream();
-        }
-    }
-    
-    private class ConstructedOctetStream
-        extends InputStream
-    {
-        private final Asn1InputStream _aIn;
-        
-        private boolean               _first = true;
-        private InputStream           _currentStream;
-        
-        ConstructedOctetStream(
-            InputStream in)
-        {
-            _aIn = new Asn1InputStream(in);
-        }
-
-        public int read() 
-            throws IOException
-        {
-            if (_first)
-            {
-                DerOctetString s = (DerOctetString)_aIn.readObject();
-
-                if (s == null)
-                {
-                    return -1;
-                }
-                
-                _first = false;
-                _currentStream = s.getOctetStream();
-            }
-            
-            int b = _currentStream.read();
-
-            if (b < 0)
-            {
-                DerOctetString s = (DerOctetString)_aIn.readObject();
-                
-                if (s == null)
-                {
-                    _currentStream = null;
-                    
-                    return -1;
-                }
-                
-                _currentStream = s.getOctetStream();
-                
-                return _currentStream.read();
-            }
-            else
-            {
-                return b;
-            }
         }
     }
 }
