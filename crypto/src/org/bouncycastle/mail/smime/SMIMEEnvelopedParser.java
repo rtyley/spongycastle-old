4bb408d123bb5420eff59529ecc26aae34a31afc
bouncy <bouncy>
2006-03-03 22:15:30 +0000

    added buffering constructors

--- crypto/src/org/bouncycastle/mail/smime/SMIMEEnvelopedParser.java
+++ crypto/src/org/bouncycastle/mail/smime/SMIMEEnvelopedParser.java
@@ -1,5 +1,6 @@
 package org.bouncycastle.mail.smime;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -18,29 +19,76 @@ import org.bouncycastle.cms.CMSException;
 public class SMIMEEnvelopedParser
     extends CMSEnvelopedDataParser
 {
-    MimePart                message;
+    private final MimePart message;
 
     private static InputStream getInputStream(
-        Part    bodyPart)
-        throws IOException, MessagingException
+        Part    bodyPart,
+        int     bufferSize)
+        throws MessagingException
     {
-        return bodyPart.getInputStream();
+        try
+        {
+            InputStream in = bodyPart.getInputStream();
+            
+            if (bufferSize == 0)
+            {
+                return new BufferedInputStream(in);
+            }
+            else
+            {
+                return new BufferedInputStream(in, bufferSize);
+            }
+        }
+        catch (IOException e)
+        {
+            throw new MessagingException("can't extract input stream: " + e);
+        }
     }
 
     public SMIMEEnvelopedParser(
         MimeBodyPart    message) 
-        throws IOException, CMSException, MessagingException
+        throws IOException, MessagingException, CMSException
     {
-        super(getInputStream(message));
-
-        this.message = message;
+        this(message, 0);
     }
 
     public SMIMEEnvelopedParser(
         MimeMessage    message) 
-        throws IOException, CMSException, MessagingException
+        throws IOException, MessagingException, CMSException
     {
-        super(getInputStream(message));
+        this(message, 0);
+    }
+    
+    /**
+     * Create a parser from a MimeBodyPart using the passed in buffer size
+     * for reading it.
+     * 
+     * @param message body part to be parsed.
+     * @param bufferSize bufferSoze to be used.
+     */
+    public SMIMEEnvelopedParser(
+        MimeBodyPart    message,
+        int             bufferSize) 
+        throws IOException, MessagingException, CMSException
+    {
+        super(getInputStream(message, bufferSize));
+
+        this.message = message;
+    }
+
+    /**
+     * Create a parser from a MimeMessage using the passed in buffer size
+     * for reading it.
+     * 
+     * @param message message to be parsed.
+     * @param bufferSize bufferSoze to be used.
+     */
+    public SMIMEEnvelopedParser(
+        MimeMessage    message,
+        int            bufferSize) 
+        throws IOException, MessagingException, CMSException
+    {
+        super(getInputStream(message, bufferSize));
 
         this.message = message;
     }
