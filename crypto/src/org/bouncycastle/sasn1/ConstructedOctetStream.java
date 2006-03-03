153c687e99de0087df362368b939e3a498aa115e
bouncy <bouncy>
2006-03-03 22:05:13 +0000

    fixed error in handling of DER constructed octet strings

---
+++ crypto/src/org/bouncycastle/sasn1/ConstructedOctetStream.java
@@ -0,0 +1,62 @@
+package org.bouncycastle.sasn1;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+class ConstructedOctetStream
+    extends InputStream
+{
+    private final Asn1InputStream _aIn;
+    
+    private boolean               _first = true;
+    private InputStream           _currentStream;
+    
+    ConstructedOctetStream(
+        InputStream in)
+    {
+        _aIn = new Asn1InputStream(in);
+    }
+    
+    public int read() 
+        throws IOException
+    {
+        if (_first)
+        {
+            Asn1OctetString s = (Asn1OctetString)_aIn.readObject();
+    
+            if (s == null)
+            {
+                return -1;
+            }
+            
+            _first = false;
+            _currentStream = s.getOctetStream();
+        }
+        else if (_currentStream == null)
+        {
+            return -1;
+        }
+            
+        int b = _currentStream.read();
+    
+        if (b < 0)
+        {
+            Asn1OctetString s = (Asn1OctetString)_aIn.readObject();
+            
+            if (s == null)
+            {
+                _currentStream = null;
+                
+                return -1;
+            }
+            
+            _currentStream = s.getOctetStream();
+            
+            return _currentStream.read();
+        }
+        else
+        {
+            return b;
+        }
+    }
+}
