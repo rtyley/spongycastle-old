4416636a5c70c67e7e629d6c75273955406509d0
bouncy <bouncy>
2006-04-26 11:27:08 +0000

    added nextLong, added zero byte padding for int and long

--- crypto/src/org/bouncycastle/util/test/FixedSecureRandom.java
+++ crypto/src/org/bouncycastle/util/test/FixedSecureRandom.java
@@ -48,11 +48,68 @@ public class FixedSecureRandom
     //
     public int nextInt()
     {
-        int val = _data[_index++] << 24;
+        int val = 0;
         
-        val |= (_data[_index++] & 0xff) << 16;
-        val |= (_data[_index++] & 0xff) << 8;
-        val |= _data[_index++] & 0xff;
+        if (_index <= _data.length - 4)
+        {
+            val |= _data[_index++] << 24;
+        }
+        if (_index <= _data.length - 3)
+        {
+            val |= (_data[_index++] & 0xff) << 16;
+        }
+        if (_index <= _data.length - 2)
+        {
+            val |= (_data[_index++] & 0xff) << 8;
+        }
+        if (_index <= _data.length - 1)
+        {
+            val |= _data[_index++] & 0xff;
+        }
+        
+        return val;
+    }
+    
+    //
+    // classpath's implementation of SecureRandom doesn't currently go back to nextBytes
+    // when next is called. We can't override next as it's a final method.
+    //
+    public long nextLong()
+    {
+        long val = 0;
+        
+        if (_index <= _data.length - 8)
+        {
+            val |= (_data[_index++] & 0xff) << 56;
+        }
+        if (_index <= _data.length - 7)
+        {
+            val |= (_data[_index++] & 0xff) << 48;
+        }
+        if (_index <= _data.length - 6)
+        {
+            val |= (_data[_index++] & 0xff) << 40;
+        }
+        if (_index <= _data.length - 5)
+        {
+            val |= (_data[_index++] & 0xff) << 32;
+        }
+        if (_index <= _data.length - 4)
+        {
+            val |= (_data[_index++] & 0xff) << 24;
+        }
+        if (_index <= _data.length - 3)
+        {
+            val |= (_data[_index++] & 0xff) << 16;
+        }
+        if (_index <= _data.length - 2)
+        {
+            val |= (_data[_index++] & 0xff) << 8;
+        }
+        if (_index <= _data.length - 1)
+        {
+            val |= _data[_index++] & 0xff;
+        }
         
         return val;
     }
