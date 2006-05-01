9cdb176130858f143782534a051048b3a084deae
bouncy <bouncy>
2006-05-01 12:53:21 +0000

    replaced use of String with DERObjectIdentifier

--- crypto/src/org/bouncycastle/asn1/x509/X509Attributes.java
+++ crypto/src/org/bouncycastle/asn1/x509/X509Attributes.java
@@ -1,6 +1,8 @@
 package org.bouncycastle.asn1.x509;
 
+import org.bouncycastle.asn1.DERObjectIdentifier;
+
 public class X509Attributes
 {
-    public static final String RoleSyntax = "2.5.4.72";
+    public static final DERObjectIdentifier RoleSyntax = new DERObjectIdentifier("2.5.4.72");
 }
