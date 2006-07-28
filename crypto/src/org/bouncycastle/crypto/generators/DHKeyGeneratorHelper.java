59d86a871bd8eb5981cc3bdd9f015ff40e41dfc9
bouncy <bouncy>
2006-07-28 09:49:44 +0000

    added fallback to prevent potential loop

--- crypto/src/org/bouncycastle/crypto/generators/DHKeyGeneratorHelper.java
+++ crypto/src/org/bouncycastle/crypto/generators/DHKeyGeneratorHelper.java
@@ -5,6 +5,8 @@ import java.security.SecureRandom;
 
 class DHKeyGeneratorHelper
 {
+    private static final int MAX_ITERATIONS = 1000;
+
     static final DHKeyGeneratorHelper INSTANCE = new DHKeyGeneratorHelper();
     
     private static BigInteger ZERO = BigInteger.valueOf(0);
@@ -24,13 +26,7 @@ class DHKeyGeneratorHelper
         
         if (limit == 0)
         {
-            int pLength = p.bitLength();
-            
-            do
-            {
-                x = new BigInteger(pLength, random);
-            }
-            while (x.equals(ZERO) || x.compareTo(pSub2) > 0);
+            x = createInRange(pSub2, random);
         }
         else
         {
@@ -39,6 +35,27 @@ class DHKeyGeneratorHelper
                 x = new BigInteger(limit, random);
             }
             while (x.equals(ZERO));
+        }
+        
+        return x;
+    }
+
+    private BigInteger createInRange(BigInteger max, SecureRandom random)
+    {
+        BigInteger x;
+        int maxLength = max.bitLength();
+        int count = 0;
+        
+        do
+        {
+            x = new BigInteger(maxLength, random);
+            count++;
+        }
+        while ((x.equals(ZERO) || x.compareTo(max) > 0) && count != MAX_ITERATIONS);
+        
+        if (count == MAX_ITERATIONS)  // fall back to a faster (restricted) method
+        {
+            return new BigInteger(maxLength - 1, random).setBit(0);
         }
         
         return x;
