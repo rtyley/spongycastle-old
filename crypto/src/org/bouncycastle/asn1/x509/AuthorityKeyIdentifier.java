cdcae32fb3739bae97e28e01ab1effac3d082717
bouncy <bouncy>
2006-08-30 00:36:17 +0000

    further JavaDoc tweaking...

--- crypto/src/org/bouncycastle/asn1/x509/AuthorityKeyIdentifier.java
+++ crypto/src/org/bouncycastle/asn1/x509/AuthorityKeyIdentifier.java
@@ -84,7 +84,7 @@ public class AuthorityKeyIdentifier
      * Example of making a AuthorityKeyIdentifier:
      * <pre>
      *   SubjectPublicKeyInfo apki = new SubjectPublicKeyInfo((ASN1Sequence)new ASN1InputStream(
-     *       new ByteArrayInputStream(publicKey.getEncoded())).readObject());
+     *       publicKey.getEncoded()).readObject());
      *   AuthorityKeyIdentifier aki = new AuthorityKeyIdentifier(apki);
      * </pre>
      *
