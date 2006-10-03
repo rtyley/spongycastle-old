79a3bf3e1d793eeb4981fa9ab58c0185565951e6
bouncy <bouncy>
2006-10-03 10:07:34 +0000

    Add some more potentially useful BigInteger constants

--- crypto/src/org/bouncycastle/math/ec/ECConstants.java
+++ crypto/src/org/bouncycastle/math/ec/ECConstants.java
@@ -6,4 +6,7 @@ public interface ECConstants
 {
     public static final BigInteger ZERO = BigInteger.valueOf(0);
     public static final BigInteger ONE = BigInteger.valueOf(1);
+    public static final BigInteger TWO = BigInteger.valueOf(2);
+    public static final BigInteger THREE = BigInteger.valueOf(3);
+    public static final BigInteger FOUR = BigInteger.valueOf(4);
 }
