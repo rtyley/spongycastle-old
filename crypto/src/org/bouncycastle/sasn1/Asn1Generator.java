d7640c922ef2bbc2d609ea64ad7fd905e08f82d6
bouncy <bouncy>
2006-10-11 10:39:29 +0000

    deprecated files

--- crypto/src/org/bouncycastle/sasn1/Asn1Generator.java
+++ crypto/src/org/bouncycastle/sasn1/Asn1Generator.java
@@ -2,6 +2,9 @@ package org.bouncycastle.sasn1;
 
 import java.io.OutputStream;
 
+/**
+ * @deprecated use org.bouncycastle.asn1.ASN1Generator
+ */
 public abstract class Asn1Generator
 {
     protected OutputStream _out;
