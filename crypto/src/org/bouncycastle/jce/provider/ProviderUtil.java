4101cae89a1a43bcb96166e9c6f7ec79465981bb
bouncy <bouncy>
2006-11-02 08:47:59 +0000

    added permission checking if security manager present.

--- crypto/src/org/bouncycastle/jce/provider/ProviderUtil.java
+++ crypto/src/org/bouncycastle/jce/provider/ProviderUtil.java
@@ -1,29 +1,79 @@
 package org.bouncycastle.jce.provider;
 
+import org.bouncycastle.jce.ProviderConfigurationPermission;
 import org.bouncycastle.jce.interfaces.ConfigurableProvider;
 import org.bouncycastle.jce.spec.ECParameterSpec;
 
+import java.security.Permission;
+
 class ProviderUtil
 {
-    private static volatile ECParameterSpec implicitlyCaCurve;
+    private static Permission BC_EC_LOCAL_PERMISSION = new ProviderConfigurationPermission(
+                                                   "BC", ConfigurableProvider.THREAD_LOCAL_EC_IMPLICITLY_CA);
+    private static Permission BC_EC_PERMISSION = new ProviderConfigurationPermission(
+                                                   "BC", ConfigurableProvider.EC_IMPLICITLY_CA);
+
+    private static ThreadLocal threadSpec = new ThreadLocal();
+    private static volatile ECParameterSpec ecImplicitCaParams;
 
     static void setParameter(String parameterName, Object parameter)
     {
-        if (parameterName.equals(ConfigurableProvider.EC_IMPLICITLY_CA))
+        SecurityManager securityManager = System.getSecurityManager();
+
+        if (parameterName.equals(ConfigurableProvider.THREAD_LOCAL_EC_IMPLICITLY_CA))
         {
-            if (parameter instanceof ECParameterSpec)
+            ECParameterSpec curveSpec;
+
+            if (securityManager != null)
             {
-                implicitlyCaCurve = (ECParameterSpec)parameter;
+                securityManager.checkPermission(BC_EC_LOCAL_PERMISSION);
+            }
+
+            if (parameter instanceof ECParameterSpec || parameter == null)
+            {
+                curveSpec = (ECParameterSpec)parameter;
             }
             else  // assume java.security.spec
             {
-                implicitlyCaCurve = EC5Util.convertSpec((java.security.spec.ECParameterSpec)parameter, false);
+                curveSpec = EC5Util.convertSpec((java.security.spec.ECParameterSpec)parameter, false);
+            }
+
+            if (curveSpec == null)
+            {
+                threadSpec.remove();
+            }
+            else
+            {
+                threadSpec.set(curveSpec);
+            }
+        }
+        else if (parameterName.equals(ConfigurableProvider.EC_IMPLICITLY_CA))
+        {
+            if (securityManager != null)
+            {
+                securityManager.checkPermission(BC_EC_PERMISSION);
+            }
+
+            if (parameter instanceof ECParameterSpec || parameter == null)
+            {
+                ecImplicitCaParams = (ECParameterSpec)parameter;
+            }
+            else  // assume java.security.spec
+            {
+                ecImplicitCaParams = EC5Util.convertSpec((java.security.spec.ECParameterSpec)parameter, false);
             }
         }
     }
 
     static ECParameterSpec getEcImplicitlyCa()
     {
-        return implicitlyCaCurve;
+        ECParameterSpec spec = (ECParameterSpec)threadSpec.get();
+
+        if (spec != null)
+        {
+            return spec;
+        }
+
+        return ecImplicitCaParams;
     }
 }
