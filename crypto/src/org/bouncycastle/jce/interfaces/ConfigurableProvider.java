697f5b86353c0cb93e167047aef333baf4a96f8d
bouncy <bouncy>
2006-11-02 08:50:40 +0000

    added thread local field.

--- crypto/src/org/bouncycastle/jce/interfaces/ConfigurableProvider.java
+++ crypto/src/org/bouncycastle/jce/interfaces/ConfigurableProvider.java
@@ -6,6 +6,7 @@ package org.bouncycastle.jce.interfaces;
  */
 public interface ConfigurableProvider
 {
+    static final String      THREAD_LOCAL_EC_IMPLICITLY_CA = "threadLocalEcImplicitlyCa";   
     static final String      EC_IMPLICITLY_CA = "ecImplicitlyCa";
 
     void setParameter(String parameterName, Object parameter);
