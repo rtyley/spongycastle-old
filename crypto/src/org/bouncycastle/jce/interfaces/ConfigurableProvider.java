10d68bb1ecbc30898f194060b94884b2ee391a15
bouncy <bouncy>
2006-11-12 03:59:56 +0000

    Fix up comment

--- crypto/src/org/bouncycastle/jce/interfaces/ConfigurableProvider.java
+++ crypto/src/org/bouncycastle/jce/interfaces/ConfigurableProvider.java
@@ -2,7 +2,7 @@ package org.bouncycastle.jce.interfaces;
 
 /**
  * Implemented by the BC provider. This allows setting of hidden parameters,
- * such as the ImplicitCA parameters from X.962if used.
+ * such as the ImplicitCA parameters from X.962, if used.
  */
 public interface ConfigurableProvider
 {
