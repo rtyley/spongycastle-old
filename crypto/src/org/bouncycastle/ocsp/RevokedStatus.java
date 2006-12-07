e161badbecaeb2e88b78d7e8f01a25dd5fe3e416
bouncy <bouncy>
2006-12-07 02:16:11 +0000

    fixed date parsing

--- crypto/src/org/bouncycastle/ocsp/RevokedStatus.java
+++ crypto/src/org/bouncycastle/ocsp/RevokedStatus.java
@@ -1,12 +1,11 @@
 package org.bouncycastle.ocsp;
 
-import java.text.ParsePosition;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.bouncycastle.asn1.DERGeneralizedTime;
 import org.bouncycastle.asn1.ocsp.RevokedInfo;
 import org.bouncycastle.asn1.x509.CRLReason;
+
+import java.text.ParseException;
+import java.util.Date;
 
 /**
  * wrapper for the RevokedInfo object
@@ -31,9 +30,14 @@ public class RevokedStatus
 
     public Date getRevocationTime()
     {
-        SimpleDateFormat dateF = new SimpleDateFormat("yyyyMMddHHmmssz");
-
-        return dateF.parse(info.getRevocationTime().getTime(), new ParsePosition(0));
+        try
+        {
+            return info.getRevocationTime().getDate();
+        }
+        catch (ParseException e)
+        {
+            throw new IllegalStateException("ParseException:" + e.getMessage());
+        }
     }
 
     public boolean hasRevocationReason()
@@ -44,6 +48,7 @@ public class RevokedStatus
     /**
      * return the revocation reason. Note: this field is optional, test for it
      * with hasRevocationReason() first.
+     * @return the revocation reason value.
      * @exception IllegalStateException if a reason is asked for and none is avaliable
      */
     public int getRevocationReason()
