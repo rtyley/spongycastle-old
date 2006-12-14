6122397a1b4f52a22dc47f97d6ce80cb0dc94391
bouncy <bouncy>
2006-12-14 03:39:26 +0000

    updates for pem reader testing

--- crypto/test/src/org/bouncycastle/openssl/test/ReaderTest.java
+++ crypto/test/src/org/bouncycastle/openssl/test/ReaderTest.java
@@ -1,5 +1,14 @@
 package org.bouncycastle.openssl.test;
 
+import org.bouncycastle.asn1.cms.CMSObjectIdentifiers;
+import org.bouncycastle.asn1.cms.ContentInfo;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
+import org.bouncycastle.openssl.PEMReader;
+import org.bouncycastle.openssl.PEMWriter;
+import org.bouncycastle.openssl.PasswordFinder;
+import org.bouncycastle.util.test.SimpleTest;
+
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -12,14 +21,7 @@ import java.security.KeyPairGenerator;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Security;
-
-import org.bouncycastle.asn1.cms.CMSObjectIdentifiers;
-import org.bouncycastle.asn1.cms.ContentInfo;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openssl.PEMReader;
-import org.bouncycastle.openssl.PEMWriter;
-import org.bouncycastle.openssl.PasswordFinder;
-import org.bouncycastle.util.test.SimpleTest;
+import java.security.Signature;
 
 /**
  * basic class for reading test.pem - the password is "secret"
@@ -56,12 +58,13 @@ public class ReaderTest
         PasswordFinder  pGet = new Password("secret".toCharArray());
         PEMReader       pemRd = new PEMReader(fRd, pGet);
         Object          o;
+        KeyPair         pair;
 
         while ((o = pemRd.readObject()) != null)
         {
             if (o instanceof KeyPair)
             {
-                KeyPair     pair = (KeyPair)o;
+                //pair = (KeyPair)o;
         
                 //System.out.println(pair.getPublic());
                 //System.out.println(pair.getPrivate());
@@ -84,12 +87,40 @@ public class ReaderTest
         {
             fail("failed envelopedData check");
         }
-        
+
+        //
+        // ECKey
+        //
+        fRd = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("eckey.pem")));
+        pemRd = new PEMReader(fRd);
+        ECNamedCurveParameterSpec spec = (ECNamedCurveParameterSpec)pemRd.readObject();
+
+        pair = (KeyPair)pemRd.readObject();
+        Signature sgr = Signature.getInstance("ECDSA", "BC");
+
+        sgr.initSign(pair.getPrivate());
+
+        byte[] message = new byte[] { (byte)'a', (byte)'b', (byte)'c' };
+
+        sgr.update(message);
+
+        byte[]  sigBytes = sgr.sign();
+
+        sgr.initVerify(pair.getPublic());
+
+        sgr.update(message);
+
+        if (!sgr.verify(sigBytes))
+        {
+            fail("EC verification failed");
+        }
+
         //
         // writer/parser test
         //
         KeyPairGenerator      kpGen = KeyPairGenerator.getInstance("RSA", "BC");
-        KeyPair               pair = kpGen.generateKeyPair();
+        
+        pair = kpGen.generateKeyPair();
         
         keyPairTest("RSA", pair);
         
