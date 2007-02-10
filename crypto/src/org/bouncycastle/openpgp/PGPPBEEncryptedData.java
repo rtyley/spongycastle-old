18638d7bed5b4611c621a1afe5f36e0bf9c4ff22
bouncy <bouncy>
2007-02-10 05:32:26 +0000

    added support for reading mixed encryption - pbe and asymmetric

--- crypto/src/org/bouncycastle/openpgp/PGPPBEEncryptedData.java
+++ crypto/src/org/bouncycastle/openpgp/PGPPBEEncryptedData.java
@@ -1,21 +1,21 @@
 package org.bouncycastle.openpgp;
 
-import java.io.EOFException;
-import java.io.InputStream;
-import java.security.DigestInputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchProviderException;
-
-import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
-
 import org.bouncycastle.bcpg.BCPGInputStream;
 import org.bouncycastle.bcpg.HashAlgorithmTags;
 import org.bouncycastle.bcpg.InputStreamPacket;
 import org.bouncycastle.bcpg.SymmetricEncIntegrityPacket;
 import org.bouncycastle.bcpg.SymmetricKeyEncSessionPacket;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.EOFException;
+import java.io.InputStream;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchProviderException;
 
 /**
  * A password based encryption object.
@@ -57,23 +57,104 @@ public class PGPPBEEncryptedData
         char[]                passPhrase,
         String                provider)
         throws PGPException, NoSuchProviderException
-    {        
+    {
+        try
+        {
+            SecretKey    key = PGPUtil.makeKeyFromPassPhrase(keyData.getEncAlgorithm(), keyData.getS2K(), passPhrase, provider);
+            int          keyAlgorithm = keyData.getEncAlgorithm();
+
+            if (keyData.getSecKeyData() != null)
+            {
+                Cipher keyCipher = Cipher.getInstance(
+                    PGPUtil.getSymmetricCipherName(keyData.getEncAlgorithm()) + "/CFB/NoPadding",
+                    provider);
+
+                keyCipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(new byte[keyCipher.getBlockSize()]));
+
+                byte[] keyBytes = keyCipher.doFinal(keyData.getSecKeyData());
+
+                keyAlgorithm = keyBytes[0];
+                key = new SecretKeySpec(keyBytes, 1, keyBytes.length - 1, PGPUtil.getSymmetricCipherName(keyBytes[0]));
+            }
+
+            Cipher c = createStreamCipher(keyAlgorithm, provider);
+
+            byte[] iv = new byte[c.getBlockSize()];
+
+            c.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+
+            encStream = new BCPGInputStream(new CipherInputStream(encData.getInputStream(), c));
+
+            if (encData instanceof SymmetricEncIntegrityPacket)
+            {
+                truncStream = new TruncatedStream(encStream);
+                encStream = new DigestInputStream(truncStream, MessageDigest.getInstance(PGPUtil.getDigestName(HashAlgorithmTags.SHA1), provider));
+            }
+
+            for (int i = 0; i != iv.length; i++)
+            {
+                int    ch = encStream.read();
+
+                if (ch < 0)
+                {
+                    throw new EOFException("unexpected end of stream.");
+                }
+
+                iv[i] = (byte)ch;
+            }
+
+            int    v1 = encStream.read();
+            int    v2 = encStream.read();
+
+            if (v1 < 0 || v2 < 0)
+            {
+                throw new EOFException("unexpected end of stream.");
+            }
+
+
+            // Note: the oracle attack on "quick check" bytes is not deemed
+            // a security risk for PBE (see PGPPublicKeyEncryptedData)
+
+            boolean repeatCheckPassed = iv[iv.length - 2] == (byte) v1
+                    && iv[iv.length - 1] == (byte) v2;
+
+            // Note: some versions of PGP appear to produce 0 for the extra
+            // bytes rather than repeating the two previous bytes
+            boolean zeroesCheckPassed = v1 == 0 && v2 == 0;
+
+            if (!repeatCheckPassed && !zeroesCheckPassed)
+            {
+                throw new PGPDataValidationException("data check failed.");
+            }
+
+            return encStream;
+        }
+        catch (PGPException e)
+        {
+            throw e;
+        }
+        catch (Exception e)
+        {
+            throw new PGPException("Exception creating cipher", e);
+        }
+    }
+
+    private Cipher createStreamCipher(int keyAlgorithm, String provider)
+        throws NoSuchProviderException, PGPException
+    {
         Cipher c;
-        
         try
         {
             if (encData instanceof SymmetricEncIntegrityPacket)
             {
-                c =
-                    Cipher.getInstance(
-                        PGPUtil.getSymmetricCipherName(keyData.getEncAlgorithm()) + "/CFB/NoPadding",
+                c = Cipher.getInstance(
+                        PGPUtil.getSymmetricCipherName(keyAlgorithm) + "/CFB/NoPadding",
                         provider);
             }
             else
             {
-                c =
-                    Cipher.getInstance(
-                        PGPUtil.getSymmetricCipherName(keyData.getEncAlgorithm()) + "/OpenPGPCFB/NoPadding",
+                c = Cipher.getInstance(
+                        PGPUtil.getSymmetricCipherName(keyAlgorithm) + "/OpenPGPCFB/NoPadding",
                         provider);
             }
         }
@@ -89,75 +170,7 @@ public class PGPPBEEncryptedData
         {
             throw new PGPException("exception creating cipher", e);
         }
-        
-        if (c != null)
-        {
-            try
-            {
-                SecretKey    key = PGPUtil.makeKeyFromPassPhrase(keyData.getEncAlgorithm(), keyData.getS2K(), passPhrase, provider);
-                
-                byte[]       iv = new byte[c.getBlockSize()];
-                
-                c.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
 
-                encStream = new BCPGInputStream(new CipherInputStream(encData.getInputStream(), c));
-                
-                if (encData instanceof SymmetricEncIntegrityPacket)
-                {
-                    truncStream = new TruncatedStream(encStream);
-                    encStream = new DigestInputStream(truncStream, MessageDigest.getInstance(PGPUtil.getDigestName(HashAlgorithmTags.SHA1), provider));
-                }
-                
-                for (int i = 0; i != iv.length; i++)
-                {
-                    int    ch = encStream.read();
-                    
-                    if (ch < 0)
-                    {
-                        throw new EOFException("unexpected end of stream.");
-                    }
-                    
-                    iv[i] = (byte)ch;
-                }
-                
-                int    v1 = encStream.read();
-                int    v2 = encStream.read();
-                
-                if (v1 < 0 || v2 < 0)
-                {
-                    throw new EOFException("unexpected end of stream.");
-                }
-
-
-                // Note: the oracle attack on "quick check" bytes is not deemed
-                // a security risk for PBE (see PGPPublicKeyEncryptedData)
-
-                boolean repeatCheckPassed = iv[iv.length - 2] == (byte) v1
-                        && iv[iv.length - 1] == (byte) v2;
-
-                // Note: some versions of PGP appear to produce 0 for the extra
-                // bytes rather than repeating the two previous bytes
-                boolean zeroesCheckPassed = v1 == 0 && v2 == 0;
-
-                if (!repeatCheckPassed && !zeroesCheckPassed)
-                {
-                    throw new PGPDataValidationException("data check failed.");
-                }
-
-                return encStream;
-            }
-            catch (PGPException e)
-            {
-                throw e;
-            }
-            catch (Exception e)
-            { 
-                throw new PGPException("Exception creating cipher", e);
-            }
-        }
-        else
-        {
-            return encData.getInputStream();
-        }
+        return c;
     }
 }
--- crypto/test/src/org/bouncycastle/openpgp/test/PGPPBETest.java
+++ crypto/test/src/org/bouncycastle/openpgp/test/PGPPBETest.java
@@ -1,12 +1,5 @@
 package org.bouncycastle.openpgp.test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.security.SecureRandom;
-import java.security.Security;
-import java.util.Date;
-
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openpgp.PGPCompressedData;
 import org.bouncycastle.openpgp.PGPCompressedDataGenerator;
@@ -18,8 +11,16 @@ import org.bouncycastle.openpgp.PGPLiteralDataGenerator;
 import org.bouncycastle.openpgp.PGPObjectFactory;
 import org.bouncycastle.openpgp.PGPPBEEncryptedData;
 import org.bouncycastle.util.encoders.Base64;
+import org.bouncycastle.util.encoders.Hex;
 import org.bouncycastle.util.test.SimpleTest;
 import org.bouncycastle.util.test.UncloseableOutputStream;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.util.Date;
 
 public class PGPPBETest
     extends SimpleTest
@@ -33,6 +34,25 @@ public class PGPPBETest
     byte[] enc1crc = Base64.decode("H66L");
 
     char[] pass = { 'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd' };
+
+    /**
+     * Message with both PBE and symmetric
+     */
+    byte[] testPBEAsym = Base64.decode(
+        "hQIOA/ZlQEFWB5vuEAf/covEUaBve7NlWWdiO5NZubdtTHGElEXzG9hyBycp9At8" +
+        "nZGi27xOZtEGFQo7pfz4JySRc3O0s6w7PpjJSonFJyNSxuze2LuqRwFWBYYcbS8/" +
+        "7YcjB6PqutrT939OWsozfNqivI9/QyZCjBvFU89pp7dtUngiZ6MVv81ds2I+vcvk" +
+        "GlIFcxcE1XoCIB3EvbqWNaoOotgEPT60unnB2BeDV1KD3lDRouMIYHfZ3SzBwOOI" +
+        "6aK39sWnY5sAK7JjFvnDAMBdueOiI0Fy+gxbFD/zFDt4cWAVSAGTC4w371iqppmT" +
+        "25TM7zAtCgpiq5IsELPlUZZnXKmnYQ7OCeysF0eeVwf+OFB9fyvCEv/zVQocJCg8" +
+        "fWxfCBlIVFNeNQpeGygn/ZmRaILvB7IXDWP0oOw7/F2Ym66IdYYIp2HeEZv+jFwa" +
+        "l41w5W4BH/gtbwGjFQ6CvF/m+lfUv6ZZdzsMIeEOwhP5g7rXBxrbcnGBaU+PXbho" +
+        "gjDqaYzAWGlrmAd6aPSj51AGeYXkb2T1T/yoJ++M3GvhH4C4hvitamDkksh/qRnM" +
+        "M/s8Nku6z1+RXO3M6p5QC1nlAVqieU8esT43945eSoC77K8WyujDNbysDyUCUTzt" +
+        "p/aoQwe/HgkeOTJNelKR9y2W3xinZLFzep0SqpNI/e468yB/2/LGsykIyQa7JX6r" +
+        "BYwuBAIDAkOKfv5rK8v0YDfnN+eFqwhTcrfBj5rDH7hER6nW3lNWcMataUiHEaMg" +
+        "o6Q0OO1vptIGxW8jClTD4N1sCNwNu9vKny8dKYDDHbCjE06DNTv7XYVW3+JqTL5E" +
+        "BnidvGgOmA==");
 
     /**
      * decrypt the passed in message stream
@@ -224,6 +244,35 @@ public class PGPPBETest
         if (!areEqual(out, test))
         {
             fail("wrong plain text in generated packet");
+        }
+
+        //
+        // sample message
+        //
+        PGPObjectFactory pgpFact = new PGPObjectFactory(testPBEAsym);
+
+        PGPEncryptedDataList enc = (PGPEncryptedDataList)pgpFact.nextObject();
+
+        PGPPBEEncryptedData     pbe = (PGPPBEEncryptedData)enc.get(1);
+
+        InputStream clear = pbe.getDataStream("password".toCharArray(), "BC");
+
+        pgpFact = new PGPObjectFactory(clear);
+
+        PGPLiteralData          ld = (PGPLiteralData)pgpFact.nextObject();
+
+        bOut = new ByteArrayOutputStream();
+        InputStream    unc = ld.getInputStream();
+        int    ch;
+
+        while ((ch = unc.read()) >= 0)
+        {
+            bOut.write(ch);
+        }
+
+        if (!areEqual(bOut.toByteArray(), Hex.decode("5361742031302e30322e30370d0a")))
+        {
+            fail("data mismatch on combined PBE");
         }
     }
 
