bbe4dbfba1b81f25fefc1419279e54c637ee56ee
bouncy <bouncy>
2007-02-15 04:08:42 +0000

    refactoring, added lookup table for some digest lenghths

--- crypto/src/org/bouncycastle/tsp/TSPUtil.java
+++ crypto/src/org/bouncycastle/tsp/TSPUtil.java
@@ -1,9 +1,7 @@
 package org.bouncycastle.tsp;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.security.cert.X509Certificate;
-
+import org.bouncycastle.asn1.ASN1InputStream;
+import org.bouncycastle.asn1.ASN1OctetString;
 import org.bouncycastle.asn1.cryptopro.CryptoProObjectIdentifiers;
 import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
 import org.bouncycastle.asn1.oiw.OIWObjectIdentifiers;
@@ -12,11 +10,47 @@ import org.bouncycastle.asn1.teletrust.TeleTrusTObjectIdentifiers;
 import org.bouncycastle.asn1.x509.ExtendedKeyUsage;
 import org.bouncycastle.asn1.x509.KeyPurposeId;
 import org.bouncycastle.asn1.x509.X509Extensions;
-import org.bouncycastle.asn1.ASN1InputStream;
-import org.bouncycastle.asn1.ASN1OctetString;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TSPUtil
 {
+    private static final Map digestLengths = new HashMap();
+    private static final Map digestNames = new HashMap();
+
+    static
+    {
+        digestLengths.put(PKCSObjectIdentifiers.md5.getId(), new Integer(16));
+        digestLengths.put(OIWObjectIdentifiers.idSHA1.getId(), new Integer(20));
+        digestLengths.put(NISTObjectIdentifiers.id_sha224.getId(), new Integer(28));
+        digestLengths.put(NISTObjectIdentifiers.id_sha256.getId(), new Integer(32));
+        digestLengths.put(NISTObjectIdentifiers.id_sha384.getId(), new Integer(48));
+        digestLengths.put(NISTObjectIdentifiers.id_sha512.getId(), new Integer(64));
+
+        digestNames.put(PKCSObjectIdentifiers.md5.getId(), "MD5");
+        digestNames.put(OIWObjectIdentifiers.idSHA1.getId(), "SHA1");
+        digestNames.put(NISTObjectIdentifiers.id_sha224.getId(), "SHA224");
+        digestNames.put(NISTObjectIdentifiers.id_sha256.getId(), "SHA256");
+        digestNames.put(NISTObjectIdentifiers.id_sha384.getId(), "SHA384");
+        digestNames.put(NISTObjectIdentifiers.id_sha512.getId(), "SHA512");
+        digestNames.put(PKCSObjectIdentifiers.sha1WithRSAEncryption.getId(), "SHA1");
+        digestNames.put(PKCSObjectIdentifiers.sha224WithRSAEncryption.getId(), "SHA224");
+        digestNames.put(PKCSObjectIdentifiers.sha256WithRSAEncryption.getId(), "SHA256");
+        digestNames.put(PKCSObjectIdentifiers.sha384WithRSAEncryption.getId(), "SHA384");
+        digestNames.put(PKCSObjectIdentifiers.sha512WithRSAEncryption.getId(), "SHA512");
+        digestNames.put(TeleTrusTObjectIdentifiers.ripemd128.getId(), "RIPEMD128");
+        digestNames.put(TeleTrusTObjectIdentifiers.ripemd160.getId(), "RIPEMD160");
+        digestNames.put(TeleTrusTObjectIdentifiers.ripemd256.getId(), "RIPEMD256");
+        digestNames.put(CryptoProObjectIdentifiers.gostR3411.getId(), "GOST3411");
+    }
+    
     /**
      * Validate the passed in certificate as being of the correct type to be used
      * for time stamping. To be valid it must have an ExtendedKeyUsage extension
@@ -64,76 +98,51 @@ public class TSPUtil
         }
     }
     
-    /**
+    /*
      * Return the digest algorithm using one of the standard JCA string
      * representations rather the the algorithm identifier (if possible).
      */
     static String getDigestAlgName(
         String digestAlgOID)
     {
-        if (PKCSObjectIdentifiers.md5.getId().equals(digestAlgOID))
+        String digestName = (String)digestNames.get(digestAlgOID);
+
+        if (digestName != null)
         {
-            return "MD5";
+            return digestName;
         }
-        else if (OIWObjectIdentifiers.idSHA1.getId().equals(digestAlgOID))
+
+        return digestAlgOID;
+    }
+
+    static int getDigestLength(
+        String digestAlgOID,
+        String provider)
+        throws NoSuchProviderException, TSPException
+    {
+        String digestName = TSPUtil.getDigestAlgName(digestAlgOID);
+
+        try
         {
-            return "SHA1";
+            Integer length = (Integer)digestLengths.get(digestAlgOID);
+
+            if (length != null)
+            {
+                return length.intValue();
+            }
+            
+            return MessageDigest.getInstance(digestName, provider).getDigestLength();
         }
-        else if (NISTObjectIdentifiers.id_sha224.getId().equals(digestAlgOID))
+        catch (NoSuchAlgorithmException e)
         {
-            return "SHA224";
-        }
-        else if (NISTObjectIdentifiers.id_sha256.getId().equals(digestAlgOID))
-        {
-            return "SHA256";
-        }
-        else if (NISTObjectIdentifiers.id_sha384.getId().equals(digestAlgOID))
-        {
-            return "SHA384";
-        }
-        else if (NISTObjectIdentifiers.id_sha512.getId().equals(digestAlgOID))
-        {
-            return "SHA512";
-        }
-        else if (PKCSObjectIdentifiers.sha1WithRSAEncryption.getId().equals(digestAlgOID))
-        {
-            return "SHA1";
-        }
-        else if (PKCSObjectIdentifiers.sha224WithRSAEncryption.getId().equals(digestAlgOID))
-        {
-            return "SHA224";
-        }
-        else if (PKCSObjectIdentifiers.sha256WithRSAEncryption.getId().equals(digestAlgOID))
-        {
-            return "SHA256";
-        }
-        else if (PKCSObjectIdentifiers.sha384WithRSAEncryption.getId().equals(digestAlgOID))
-        {
-            return "SHA384";
-        }
-        else if (PKCSObjectIdentifiers.sha512WithRSAEncryption.getId().equals(digestAlgOID))
-        {
-            return "SHA512";
-        }
-        else if (TeleTrusTObjectIdentifiers.ripemd128.getId().equals(digestAlgOID))
-        {
-            return "RIPEMD128";
-        }
-        else if (TeleTrusTObjectIdentifiers.ripemd160.getId().equals(digestAlgOID))
-        {
-            return "RIPEMD160";
-        }
-        else if (TeleTrusTObjectIdentifiers.ripemd256.getId().equals(digestAlgOID))
-        {
-            return "RIPEMD256";
-        }
-        else if (CryptoProObjectIdentifiers.gostR3411.getId().equals(digestAlgOID))
-        {
-            return "GOST3411";
-        }
-        else
-        {
-            return digestAlgOID;            
+            try
+            {
+                return MessageDigest.getInstance(digestName).getDigestLength();
+            }
+            catch (NoSuchAlgorithmException ex)
+            {
+                throw new TSPException("digest algorithm cannot be found.", ex);
+            }
         }
     }
 }
