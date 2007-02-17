cdd43a2a7515b5e190b66f1da6bae1a8174387a2
bouncy <bouncy>
2007-02-17 07:26:43 +0000

    added SEED testing, extended aliases

--- crypto/src/org/bouncycastle/jce/provider/symmetric/SEEDMappings.java
+++ crypto/src/org/bouncycastle/jce/provider/symmetric/SEEDMappings.java
@@ -12,10 +12,11 @@ public class SEEDMappings
         put("Cipher.SEED", "org.bouncycastle.jce.provider.symmetric.SEED$ECB");
         put("Cipher." + KISAObjectIdentifiers.id_seedCBC, "org.bouncycastle.jce.provider.symmetric.SEED$CBC");
 
-        put("Cipher.SEEDWRAP", "org.bouncycastle.jce.provider.WrapCipherSpi$Wrap");
+        put("Cipher.SEEDWRAP", "org.bouncycastle.jce.provider.symmetric.SEED$Wrap");
         put("Alg.Alias.Cipher." + KISAObjectIdentifiers.id_npki_app_cmsSeed_wrap, "SEEDWRAP");
 
         put("KeyGenerator.SEED", "org.bouncycastle.jce.provider.symmetric.SEED$KeyGen");
-        put("KeyGenerator." + KISAObjectIdentifiers.id_npki_app_cmsSeed_wrap, "org.bouncycastle.jce.provider.symmetric.SEED$KeyGen128");
+        put("KeyGenerator." + KISAObjectIdentifiers.id_seedCBC, "org.bouncycastle.jce.provider.symmetric.SEED$KeyGen");
+        put("KeyGenerator." + KISAObjectIdentifiers.id_npki_app_cmsSeed_wrap, "org.bouncycastle.jce.provider.symmetric.SEED$KeyGen");
     }
 }
