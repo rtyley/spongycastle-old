accf3bbe2d27d5a6786737977429ac4cedd1b227
bouncy <bouncy>
2007-03-09 00:17:53 +0000

    i18n updates

--- crypto/src/org/bouncycastle/i18n/ErrorBundle.java
+++ crypto/src/org/bouncycastle/i18n/ErrorBundle.java
@@ -1,5 +1,6 @@
 package org.bouncycastle.i18n;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -33,12 +34,13 @@ public class ErrorBundle extends MessageBundle
      * RessourceBundle and <code>id</code> as the message bundle id the resource file. 
      * @param resource base name of the resource file 
      * @param id the id of the corresponding bundle in the resource file
-     * @param loader the {@link ClassLoader} used to load the resource file
+     * @param encoding the encoding of the resource file
      * @throws NullPointerException if <code>resource</code> or <code>id</code> is <code>null</code>
+     * @throws UnsupportedEncodingException if the encoding is not supported
      */
-    public ErrorBundle(String resource, String id, ClassLoader loader) throws NullPointerException
+    public ErrorBundle(String resource, String id, String encoding) throws NullPointerException, UnsupportedEncodingException
     {
-        super(resource, id, loader);
+        super(resource, id, encoding);
     }
 
     /**
@@ -59,13 +61,14 @@ public class ErrorBundle extends MessageBundle
      * RessourceBundle and <code>id</code> as the message bundle id the resource file. 
      * @param resource base name of the resource file 
      * @param id the id of the corresponding bundle in the resource file
-     * @param loader the {@link ClassLoader} used to load the resource file
+     * @param encoding the encoding of the resource file
      * @param arguments an array containing the arguments for the message
      * @throws NullPointerException if <code>resource</code> or <code>id</code> is <code>null</code>
+     * @throws UnsupportedEncodingException if the encoding is not supported
      */
-    public ErrorBundle(String resource, String id, ClassLoader loader, Object[] arguments) throws NullPointerException
+    public ErrorBundle(String resource, String id, String encoding, Object[] arguments) throws NullPointerException, UnsupportedEncodingException
     {
-        super(resource, id, loader, arguments);
+        super(resource, id, encoding, arguments);
     }
     
     /**
--- crypto/src/org/bouncycastle/i18n/MissingEntryException.java
+++ crypto/src/org/bouncycastle/i18n/MissingEntryException.java
@@ -1,16 +1,36 @@
 package org.bouncycastle.i18n;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
+import java.util.Locale;
+
 public class MissingEntryException extends RuntimeException 
 {
 
     protected final String resource;
     protected final String key;
+    protected final ClassLoader loader;
+    protected final Locale locale;
+    
+    private String debugMsg;
 
-    public MissingEntryException(String message, String resource, String key) 
+    public MissingEntryException(String message, String resource, String key, Locale locale, ClassLoader loader) 
     {
         super(message);
         this.resource = resource;
         this.key = key;
+        this.locale = locale;
+        this.loader = loader;
+    }
+    
+    public MissingEntryException(String message, Throwable cause, String resource, String key, Locale locale, ClassLoader loader) 
+    {
+        super(message, cause);
+        this.resource = resource;
+        this.key = key;
+        this.locale = locale;
+        this.loader = loader;
     }
 
     public String getKey()
@@ -21,6 +41,30 @@ public class MissingEntryException extends RuntimeException
     public String getResource()
     {
         return resource;
+    }
+    
+    public ClassLoader getClassLoader()
+    {
+        return loader;
+    }
+    
+    public Locale getLocale()
+    {
+        return locale;
+    }
+
+    public String getDebugMsg()
+    {
+        if (debugMsg == null)
+        {
+            debugMsg = "Can not find entry " + key + " in resource file " + resource + " for the locale " + locale + ".";
+            if (loader instanceof URLClassLoader)
+            {
+                URL[] urls = ((URLClassLoader) loader).getURLs();
+                debugMsg += " The following entries in the classpath were searched: " + Arrays.toString(urls);
+            }
+        }
+        return debugMsg;
     }
 
 }
