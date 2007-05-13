192f5e771fa42cd0dbc3244a695c4f57cc9c3137
bouncy <bouncy>
2007-05-13 07:58:59 +0000

    Delegate to SECNamedCurves for curve creation

--- crypto/src/org/bouncycastle/asn1/nist/NISTNamedCurves.java
+++ crypto/src/org/bouncycastle/asn1/nist/NISTNamedCurves.java
@@ -15,42 +15,27 @@ import java.util.Hashtable;
 public class NISTNamedCurves
 {
     static final Hashtable objIds = new Hashtable();
-    static final Hashtable curves = new Hashtable();
     static final Hashtable names = new Hashtable();
+
+    static void defineCurve(String name, DERObjectIdentifier oid)
+    {
+        objIds.put(name, oid);
+        names.put(oid, name);
+    }
 
     static
     {
-        objIds.put("B-571", SECObjectIdentifiers.sect571r1);
-        objIds.put("B-409", SECObjectIdentifiers.sect409r1);  
-        objIds.put("B-283", SECObjectIdentifiers.sect283r1);
-        objIds.put("B-233", SECObjectIdentifiers.sect233r1);
-        objIds.put("B-163", SECObjectIdentifiers.sect163r2);       
-        objIds.put("P-521", SECObjectIdentifiers.secp521r1);       
-        objIds.put("P-256", SECObjectIdentifiers.secp256r1);   
-        objIds.put("P-224", SECObjectIdentifiers.secp224r1); 
-        objIds.put("P-384", SECObjectIdentifiers.secp384r1); 
-
-        names.put(SECObjectIdentifiers.sect571r1, "B-571"); 
-        names.put(SECObjectIdentifiers.sect409r1, "B-409");  
-        names.put(SECObjectIdentifiers.sect283r1, "B-283");
-        names.put(SECObjectIdentifiers.sect233r1, "B-233");
-        names.put(SECObjectIdentifiers.sect163r2, "B-163");       
-        names.put(SECObjectIdentifiers.secp521r1, "P-521");       
-        names.put(SECObjectIdentifiers.secp256r1, "P-256"); 
-        names.put(SECObjectIdentifiers.secp224r1, "P-224");
-        names.put(SECObjectIdentifiers.secp384r1, "P-384");
-
-        curves.put(SECObjectIdentifiers.sect571r1, SECNamedCurves.getByName("sect571r1"));
-        curves.put(SECObjectIdentifiers.sect409r1, SECNamedCurves.getByName("sect409r1")); 
-        curves.put(SECObjectIdentifiers.sect283r1, SECNamedCurves.getByName("sect283r1")); 
-        curves.put(SECObjectIdentifiers.sect233r1, SECNamedCurves.getByName("sect233r1")); 
-        curves.put(SECObjectIdentifiers.sect163r2, SECNamedCurves.getByName("sect163r2"));       
-        curves.put(SECObjectIdentifiers.secp521r1, SECNamedCurves.getByName("secp521r1")); 
-        curves.put(SECObjectIdentifiers.secp256r1, SECNamedCurves.getByName("secp256r1"));
-        curves.put(SECObjectIdentifiers.secp224r1, SECNamedCurves.getByName("secp224r1"));             
-        curves.put(SECObjectIdentifiers.secp384r1, SECNamedCurves.getByName("secp384r1"));             
+        defineCurve("B-571", SECObjectIdentifiers.sect571r1);
+        defineCurve("B-409", SECObjectIdentifiers.sect409r1);
+        defineCurve("B-283", SECObjectIdentifiers.sect283r1);
+        defineCurve("B-233", SECObjectIdentifiers.sect233r1);
+        defineCurve("B-163", SECObjectIdentifiers.sect163r2);       
+        defineCurve("P-521", SECObjectIdentifiers.secp521r1);       
+        defineCurve("P-256", SECObjectIdentifiers.secp256r1);   
+        defineCurve("P-224", SECObjectIdentifiers.secp224r1); 
+        defineCurve("P-384", SECObjectIdentifiers.secp384r1); 
     }
-    
+
     public static X9ECParameters getByName(
         String  name)
     {
@@ -58,7 +43,7 @@ public class NISTNamedCurves
 
         if (oid != null)
         {
-            return (X9ECParameters)curves.get(oid);
+            return getByOID(oid);
         }
 
         return null;
@@ -73,7 +58,7 @@ public class NISTNamedCurves
     public static X9ECParameters getByOID(
         DERObjectIdentifier  oid)
     {
-        return (X9ECParameters)curves.get(oid);
+        return SECNamedCurves.getByOID(oid);
     }
 
     /**
