b8cda512539920340ed454129c9280ccf8fb6586
bouncy <bouncy>
2007-05-16 15:15:42 +0000

    Expand X9IntegerConverter methods to enable retrieving the field size (and byte length) from either ECFieldElement or ECCurve objects

--- crypto/src/org/bouncycastle/asn1/x9/X9IntegerConverter.java
+++ crypto/src/org/bouncycastle/asn1/x9/X9IntegerConverter.java
@@ -1,28 +1,50 @@
 package org.bouncycastle.asn1.x9;
 
-import java.math.BigInteger;
-
+import org.bouncycastle.math.ec.ECCurve;
 import org.bouncycastle.math.ec.ECFieldElement;
+
+import java.math.BigInteger;
 
 public class X9IntegerConverter
 {
     public int getByteLength(
+        ECCurve c)
+    {
+        return (getFieldSize(c) + 7) / 8;
+    }
+
+    public int getByteLength(
+        ECFieldElement fe)
+    {
+        return (getFieldSize(fe) + 7) / 8;
+    }
+
+    public int getFieldSize(
+        ECCurve c)
+    {
+        if (c instanceof ECCurve.Fp)
+        {
+            return ((ECCurve.Fp) c).getQ().bitLength();
+        }
+        else
+        {
+            return ((ECCurve.F2m) c).getM();
+        }
+    }
+
+    public int getFieldSize(
         ECFieldElement fe)
     {
         if (fe instanceof ECFieldElement.Fp)
         {
-            ECFieldElement.Fp ep = (ECFieldElement.Fp)fe;
-            
-            return (ep.getQ().bitLength() + 7) / 8;
+            return ((ECFieldElement.Fp)fe).getQ().bitLength();
         }
         else
         {
-            ECFieldElement.F2m em = (ECFieldElement.F2m)fe;
-            
-            return (em.getM() + 7) / 8;
+            return ((ECFieldElement.F2m)fe).getM();
         }
     }
-    
+
     public byte[] integerToBytes(
         BigInteger s,
         int        qLength)
