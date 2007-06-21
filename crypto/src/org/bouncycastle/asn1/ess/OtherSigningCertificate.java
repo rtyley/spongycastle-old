b2278e2b92c03cef3bef5551a443124843057c37
bouncy <bouncy>
2007-06-21 08:53:04 +0000

    Sort out OIDs from RFC 3126 (CAdES)

--- crypto/src/org/bouncycastle/asn1/ess/OtherSigningCertificate.java
+++ crypto/src/org/bouncycastle/asn1/ess/OtherSigningCertificate.java
@@ -91,7 +91,7 @@ public class OtherSigningCertificate
      *      policies     SEQUENCE OF PolicyInformation OPTIONAL
      * }
      * </pre>
-     * id-aa-otherSigCert OBJECT IDENTIFIER ::= { iso(1)
+     * id-aa-ets-otherSigCert OBJECT IDENTIFIER ::= { iso(1)
      *  member-body(2) us(840) rsadsi(113549) pkcs(1) pkcs9(9)
      *  smime(16) id-aa(2) 19 }
      */
