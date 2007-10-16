b9a6b844979f43399bdc64be8ca08fac8d7d4ada
bouncy <bouncy>
2007-10-16 07:10:49 +0000

    simple improvement to set sort

--- crypto/test/src/org/bouncycastle/asn1/test/SetTest.java
+++ crypto/test/src/org/bouncycastle/asn1/test/SetTest.java
@@ -11,81 +11,105 @@ import org.bouncycastle.asn1.DEROctetString;
 import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.asn1.DERSet;
 import org.bouncycastle.asn1.DERTaggedObject;
-import org.bouncycastle.util.test.SimpleTestResult;
-import org.bouncycastle.util.test.Test;
-import org.bouncycastle.util.test.TestResult;
+import org.bouncycastle.util.test.SimpleTest;
 
 /**
  * Set sorting test example
  */
 public class SetTest
-    implements Test
+    extends SimpleTest
 {
 
     public String getName()
     {
         return "Set";
     }
-    
-    public TestResult perform()
+
+    private void checkedSortedSet(int attempt, ASN1Set s)
     {
-        try
+        if (s.getObjectAt(0) instanceof DERBoolean
+            && s.getObjectAt(1) instanceof DERInteger
+            && s.getObjectAt(2) instanceof DERBitString
+            && s.getObjectAt(3) instanceof DEROctetString)
         {
-            ASN1EncodableVector v = new ASN1EncodableVector();
-            byte[] data = new byte[10];
-         
-            v.add(new DEROctetString(data));
-            v.add(new DERBitString(data));
-            v.add(new DERInteger(100)); 
-            v.add(new DERBoolean(true));
-            
-            ASN1Set s = new DERSet(v);
-            
-            if (!(s.getObjectAt(0) instanceof DERBoolean))
-            {
-                return new SimpleTestResult(false, getName() + ": sorting failed.");
-            }
-
-            s = new BERSet(v);
-            
-            if (!(s.getObjectAt(0) instanceof DEROctetString))
-            {
-                return new SimpleTestResult(false, getName() + ": BER set sort order changed.");
-            }
-            
-            // create an implicitly tagged "set" without sorting
-            ASN1TaggedObject tag = new DERTaggedObject(false, 1, new DERSequence(v));
-            s = ASN1Set.getInstance(tag, false);
-            
-            if (s.getObjectAt(0) instanceof DERBoolean)
-            {
-                return new SimpleTestResult(false, getName() + ": sorted when shouldn't be.");
-            }
-
-            // equality test
-            v = new ASN1EncodableVector();
-
-            v.add(new DERBoolean(true));
-            v.add(new DERBoolean(true));
-            v.add(new DERBoolean(true));
-
-            s = new DERSet(v);
-            
-            return new SimpleTestResult(true, getName() + ": Okay");
+            return;
         }
-        catch (Exception e)
+
+        fail("sorting failed on attempt: " + attempt);
+    }
+
+    public void performTest()
+    {
+        ASN1EncodableVector v = new ASN1EncodableVector();
+        byte[] data = new byte[10];
+
+        v.add(new DEROctetString(data));
+        v.add(new DERBitString(data));
+        v.add(new DERInteger(100));
+        v.add(new DERBoolean(true));
+
+        checkedSortedSet(0, new DERSet(v));
+
+        v = new ASN1EncodableVector();
+        v.add(new DERInteger(100));
+        v.add(new DERBoolean(true));
+        v.add(new DEROctetString(data));
+        v.add(new DERBitString(data));
+
+        checkedSortedSet(1, new DERSet(v));
+
+        v = new ASN1EncodableVector();
+        v.add(new DERBoolean(true));
+        v.add(new DEROctetString(data));
+        v.add(new DERBitString(data));
+        v.add(new DERInteger(100));
+
+
+        checkedSortedSet(2, new DERSet(v));
+
+        v = new ASN1EncodableVector();
+        v.add(new DERBitString(data));
+        v.add(new DEROctetString(data));
+        v.add(new DERInteger(100));
+        v.add(new DERBoolean(true));
+
+        checkedSortedSet(3, new DERSet(v));
+
+        v = new ASN1EncodableVector();
+        v.add(new DEROctetString(data));
+        v.add(new DERBitString(data));
+        v.add(new DERInteger(100));
+        v.add(new DERBoolean(true));
+
+        ASN1Set s = new BERSet(v);
+
+        if (!(s.getObjectAt(0) instanceof DEROctetString))
         {
-            return new SimpleTestResult(false, getName() + ": Exception - " + e.toString(), e);
+            fail("BER set sort order changed.");
         }
+
+        // create an implicitly tagged "set" without sorting
+        ASN1TaggedObject tag = new DERTaggedObject(false, 1, new DERSequence(v));
+        s = ASN1Set.getInstance(tag, false);
+
+        if (s.getObjectAt(0) instanceof DERBoolean)
+        {
+            fail("sorted when shouldn't be.");
+        }
+
+        // equality test
+        v = new ASN1EncodableVector();
+
+        v.add(new DERBoolean(true));
+        v.add(new DERBoolean(true));
+        v.add(new DERBoolean(true));
+
+        s = new DERSet(v);
     }
 
     public static void main(
         String[]    args)
     {
-        Test    test = new SetTest();
-
-        TestResult  result = test.perform();
-
-        System.out.println(result);
+        runTest(new SetTest());
     }
 }
