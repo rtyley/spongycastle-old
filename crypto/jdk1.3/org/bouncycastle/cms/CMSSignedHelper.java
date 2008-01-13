61882ea69d8721d82b2ebdd92a67e765ae7abd63
bouncy <bouncy>
2008-01-13 03:55:16 +0000

    compatibility updates.

--- crypto/jdk1.3/org/bouncycastle/cms/CMSSignedHelper.java
+++ crypto/jdk1.3/org/bouncycastle/cms/CMSSignedHelper.java
@@ -5,6 +5,7 @@ import org.bouncycastle.asn1.ASN1Set;
 import org.bouncycastle.asn1.ASN1TaggedObject;
 import org.bouncycastle.asn1.DEREncodable;
 import org.bouncycastle.asn1.DERObject;
+import org.bouncycastle.asn1.DERObjectIdentifier;
 import org.bouncycastle.asn1.cryptopro.CryptoProObjectIdentifiers;
 import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
 import org.bouncycastle.asn1.oiw.OIWObjectIdentifiers;
@@ -41,45 +42,69 @@ class CMSSignedHelper
 
     private static final Map     encryptionAlgs = new HashMap();
     private static final Map     digestAlgs = new HashMap();
+    private static final Map     digestAliases = new HashMap();
 
+    private static void addEntries(DERObjectIdentifier alias, String digest, String encryption)
+    {
+        digestAlgs.put(alias.getId(), digest);
+        encryptionAlgs.put(alias.getId(), encryption);
+    }
 
     static
     {
-        encryptionAlgs.put(X9ObjectIdentifiers.id_dsa_with_sha1.getId(), "DSA");
+        addEntries(NISTObjectIdentifiers.dsa_with_sha224, "SHA224", "DSA");
+        addEntries(NISTObjectIdentifiers.dsa_with_sha256, "SHA256", "DSA");
+        addEntries(NISTObjectIdentifiers.dsa_with_sha384, "SHA384", "DSA");
+        addEntries(NISTObjectIdentifiers.dsa_with_sha512, "SHA512", "DSA");
+        addEntries(OIWObjectIdentifiers.dsaWithSHA1, "SHA1", "DSA");
+        addEntries(OIWObjectIdentifiers.md4WithRSA, "MD4", "RSA");
+        addEntries(OIWObjectIdentifiers.md4WithRSAEncryption, "MD4", "RSA");
+        addEntries(OIWObjectIdentifiers.md5WithRSA, "MD5", "RSA");
+        addEntries(OIWObjectIdentifiers.sha1WithRSA, "SHA1", "RSA");
+        addEntries(PKCSObjectIdentifiers.md2WithRSAEncryption, "MD2", "RSA");
+        addEntries(PKCSObjectIdentifiers.md4WithRSAEncryption, "MD4", "RSA");
+        addEntries(PKCSObjectIdentifiers.md5WithRSAEncryption, "MD5", "RSA");
+        addEntries(PKCSObjectIdentifiers.sha1WithRSAEncryption, "SHA1", "RSA");
+        addEntries(PKCSObjectIdentifiers.sha224WithRSAEncryption, "SHA224", "RSA");
+        addEntries(PKCSObjectIdentifiers.sha256WithRSAEncryption, "SHA256", "RSA");
+        addEntries(PKCSObjectIdentifiers.sha384WithRSAEncryption, "SHA384", "RSA");
+        addEntries(PKCSObjectIdentifiers.sha512WithRSAEncryption, "SHA512", "RSA");
+        addEntries(X9ObjectIdentifiers.ecdsa_with_SHA1, "SHA1", "ECDSA");
+        addEntries(X9ObjectIdentifiers.ecdsa_with_SHA224, "SHA224", "ECDSA");
+        addEntries(X9ObjectIdentifiers.ecdsa_with_SHA256, "SHA256", "ECDSA");
+        addEntries(X9ObjectIdentifiers.ecdsa_with_SHA384, "SHA384", "ECDSA");
+        addEntries(X9ObjectIdentifiers.ecdsa_with_SHA512, "SHA512", "ECDSA");
+        addEntries(X9ObjectIdentifiers.id_dsa_with_sha1, "SHA1", "DSA");
+
         encryptionAlgs.put(X9ObjectIdentifiers.id_dsa.getId(), "DSA");
-        encryptionAlgs.put(OIWObjectIdentifiers.dsaWithSHA1.getId(), "DSA");
         encryptionAlgs.put(PKCSObjectIdentifiers.rsaEncryption.getId(), "RSA");
-        encryptionAlgs.put(PKCSObjectIdentifiers.sha1WithRSAEncryption.getId(), "RSA");
         encryptionAlgs.put(TeleTrusTObjectIdentifiers.teleTrusTRSAsignatureAlgorithm, "RSA");
         encryptionAlgs.put(X509ObjectIdentifiers.id_ea_rsa.getId(), "RSA");
-        encryptionAlgs.put(CMSSignedDataGenerator.ENCRYPTION_ECDSA, "ECDSA");
-        encryptionAlgs.put(X9ObjectIdentifiers.ecdsa_with_SHA2.getId(), "ECDSA");
-        encryptionAlgs.put(X9ObjectIdentifiers.ecdsa_with_SHA224.getId(), "ECDSA");
-        encryptionAlgs.put(X9ObjectIdentifiers.ecdsa_with_SHA256.getId(), "ECDSA");
-        encryptionAlgs.put(X9ObjectIdentifiers.ecdsa_with_SHA384.getId(), "ECDSA");
-        encryptionAlgs.put(X9ObjectIdentifiers.ecdsa_with_SHA512.getId(), "ECDSA");
         encryptionAlgs.put(CMSSignedDataGenerator.ENCRYPTION_RSA_PSS, "RSAandMGF1");
         encryptionAlgs.put(CryptoProObjectIdentifiers.gostR3410_94.getId(), "GOST3410");
         encryptionAlgs.put(CryptoProObjectIdentifiers.gostR3410_2001.getId(), "ECGOST3410");
         encryptionAlgs.put("1.3.6.1.4.1.5849.1.6.2", "ECGOST3410");
         encryptionAlgs.put("1.3.6.1.4.1.5849.1.1.5", "GOST3410");
 
+        digestAlgs.put(PKCSObjectIdentifiers.md2.getId(), "MD2");
+        digestAlgs.put(PKCSObjectIdentifiers.md4.getId(), "MD4");
         digestAlgs.put(PKCSObjectIdentifiers.md5.getId(), "MD5");
         digestAlgs.put(OIWObjectIdentifiers.idSHA1.getId(), "SHA1");
         digestAlgs.put(NISTObjectIdentifiers.id_sha224.getId(), "SHA224");
         digestAlgs.put(NISTObjectIdentifiers.id_sha256.getId(), "SHA256");
         digestAlgs.put(NISTObjectIdentifiers.id_sha384.getId(), "SHA384");
         digestAlgs.put(NISTObjectIdentifiers.id_sha512.getId(), "SHA512");
-        digestAlgs.put(PKCSObjectIdentifiers.sha1WithRSAEncryption.getId(), "SHA1");
-        digestAlgs.put(PKCSObjectIdentifiers.sha224WithRSAEncryption.getId(), "SHA224");
-        digestAlgs.put(PKCSObjectIdentifiers.sha256WithRSAEncryption.getId(), "SHA256");
-        digestAlgs.put(PKCSObjectIdentifiers.sha384WithRSAEncryption.getId(), "SHA384");
-        digestAlgs.put(PKCSObjectIdentifiers.sha512WithRSAEncryption.getId(), "SHA512");
         digestAlgs.put(TeleTrusTObjectIdentifiers.ripemd128.getId(), "RIPEMD128");
         digestAlgs.put(TeleTrusTObjectIdentifiers.ripemd160.getId(), "RIPEMD160");
         digestAlgs.put(TeleTrusTObjectIdentifiers.ripemd256.getId(), "RIPEMD256");
         digestAlgs.put(CryptoProObjectIdentifiers.gostR3411.getId(),  "GOST3411");
         digestAlgs.put("1.3.6.1.4.1.5849.1.2.1",  "GOST3411");
+
+        digestAliases.put("SHA1", new String[] { "SHA-1" });
+        digestAliases.put("SHA224", new String[] { "SHA-224" });
+        digestAliases.put("SHA256", new String[] { "SHA-256" });
+        digestAliases.put("SHA384", new String[] { "SHA-384" });
+        digestAliases.put("SHA512", new String[] { "SHA-512" });
     }
     
     /**
@@ -98,7 +123,20 @@ class CMSSignedHelper
 
         return digestAlgOID;
     }
-    
+
+    String[] getDigestAliases(
+        String algName)
+    {
+        String[] aliases = (String[])digestAliases.get(algName);
+
+        if (aliases != null)
+        {
+            return aliases;
+        }
+
+        return new String[0];
+    }
+
     /**
      * Return the digest encryption algorithm using one of the standard
      * JCA string representations rather the the algorithm identifier (if
@@ -122,23 +160,47 @@ class CMSSignedHelper
         String provider) 
         throws NoSuchProviderException, NoSuchAlgorithmException
     {
+        try
+        {
+            return createDigestInstance(algorithm, provider);
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            String[] aliases = getDigestAliases(algorithm);
+            for (int i = 0; i != aliases.length; i++)
+            {
+                try
+                {
+                    return createDigestInstance(aliases[i], provider);
+                }
+                catch (NoSuchAlgorithmException ex)
+                {
+                    // continue
+                }
+            }
+            if (provider != null)
+            {
+                return getDigestInstance(algorithm, null); // try rolling back
+            }
+            throw e;
+        }
+    }
+
+    private MessageDigest createDigestInstance(
+        String algorithm,
+        String provider)
+        throws NoSuchAlgorithmException, NoSuchProviderException
+    {
         if (provider != null)
         {
-            try
-            {
-                return MessageDigest.getInstance(algorithm, provider);
-            }
-            catch (NoSuchAlgorithmException e)
-            {
-                return MessageDigest.getInstance(algorithm); // try rolling back
-            }
+            return MessageDigest.getInstance(algorithm, provider);
         }
         else
         {
             return MessageDigest.getInstance(algorithm);
         }
     }
-    
+
     Signature getSignatureInstance(
         String algorithm, 
         String provider) 
@@ -274,7 +336,14 @@ class CMSSignedHelper
 
         try
         {
-            return CertStore.getInstance(type, new CollectionCertStoreParameters(certsAndcrls), provider);
+            if (provider != null)
+            {
+                return CertStore.getInstance(type, new CollectionCertStoreParameters(certsAndcrls), provider);
+            }
+            else
+            {
+                return CertStore.getInstance(type, new CollectionCertStoreParameters(certsAndcrls));
+            }
         }
         catch (InvalidAlgorithmParameterException e)
         {
@@ -289,7 +358,14 @@ class CMSSignedHelper
 
         try
         {
-            cf = CertificateFactory.getInstance("X.509", provider);
+            if (provider != null)
+            {
+                cf = CertificateFactory.getInstance("X.509", provider);
+            }
+            else
+            {
+                cf = CertificateFactory.getInstance("X.509");
+            }
         }
         catch (CertificateException ex)
         {
@@ -329,7 +405,14 @@ class CMSSignedHelper
 
         try
         {
-            cf = CertificateFactory.getInstance("X.509", provider);
+            if (provider != null)
+            {
+                cf = CertificateFactory.getInstance("X.509", provider);
+            }
+            else
+            {
+                cf = CertificateFactory.getInstance("X.509");
+            }
         }
         catch (CertificateException ex)
         {
