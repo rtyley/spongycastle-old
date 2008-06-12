8867c0cb910b839cb7cf80140be06b3a71483b2f
bouncy <bouncy>
2008-06-12 00:47:42 +0000

    updated to use final OID value for id-icao

--- crypto/src/org/bouncycastle/asn1/icao/ICAOObjectIdentifiers.java
+++ crypto/src/org/bouncycastle/asn1/icao/ICAOObjectIdentifiers.java
@@ -7,7 +7,7 @@ public interface ICAOObjectIdentifiers
     //
     // base id
     //
-    static final String                 id_icao                   = "1.3.27";
+    static final String                 id_icao                   = "2.23.136";
 
     static final DERObjectIdentifier    id_icao_mrtd              = new DERObjectIdentifier(id_icao+".1");
     static final DERObjectIdentifier    id_icao_mrtd_security     = new DERObjectIdentifier(id_icao_mrtd+".1");
