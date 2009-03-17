eae94974316c49c38ae10b081c54eee128ead540
bouncy <bouncy>
2009-03-17 09:37:22 +0000

    Specify m=0

--- crypto/src/org/bouncycastle/crypto/generators/ElGamalKeyPairGenerator.java
+++ crypto/src/org/bouncycastle/crypto/generators/ElGamalKeyPairGenerator.java
@@ -32,7 +32,7 @@ public class ElGamalKeyPairGenerator
     {
 		DHKeyGeneratorHelper helper = DHKeyGeneratorHelper.INSTANCE;
         ElGamalParameters egp = param.getParameters();
-        DHParameters dhp = new DHParameters(egp.getP(), egp.getG(), null, egp.getL());  
+        DHParameters dhp = new DHParameters(egp.getP(), egp.getG(), null, 0, egp.getL());  
 
         BigInteger x = helper.calculatePrivate(dhp, param.getRandom()); 
         BigInteger y = helper.calculatePublic(dhp, x);
