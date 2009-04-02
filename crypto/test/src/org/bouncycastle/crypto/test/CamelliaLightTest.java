4bdb5c9e50e781c1ddc75a45468a025a74180327
bouncy <bouncy>
2009-04-02 06:48:31 +0000

    Remove unused import

--- crypto/test/src/org/bouncycastle/crypto/test/CamelliaLightTest.java
+++ crypto/test/src/org/bouncycastle/crypto/test/CamelliaLightTest.java
@@ -4,7 +4,6 @@ import org.bouncycastle.crypto.engines.CamelliaLightEngine;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.util.encoders.Hex;
 import org.bouncycastle.util.test.SimpleTest;
-import org.bouncycastle.util.test.TestResult;
 
 /**
  * Camellia tester - vectors from https://www.cosic.esat.kuleuven.be/nessie/testvectors/ and RFC 3713
