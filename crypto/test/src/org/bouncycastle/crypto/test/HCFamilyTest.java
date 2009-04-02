82b72008e6f13168a64e90fb355c8c4dfb2f03f2
bouncy <bouncy>
2009-04-02 11:52:02 +0000

    Update test to parse latest test vectors from ecrypt

--- crypto/test/src/org/bouncycastle/crypto/test/HCFamilyTest.java
+++ crypto/test/src/org/bouncycastle/crypto/test/HCFamilyTest.java
@@ -1,10 +1,17 @@
 package org.bouncycastle.crypto.test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.bouncycastle.crypto.CipherParameters;
 import org.bouncycastle.crypto.StreamCipher;
 import org.bouncycastle.crypto.engines.HC128Engine;
 import org.bouncycastle.crypto.engines.HC256Engine;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.crypto.params.ParametersWithIV;
+import org.bouncycastle.util.Arrays;
 import org.bouncycastle.util.encoders.Hex;
 import org.bouncycastle.util.test.SimpleTest;
 
@@ -18,155 +25,165 @@ import org.bouncycastle.util.test.SimpleTest;
 public class HCFamilyTest
     extends SimpleTest
 {
-    private static final byte[] MSG = new byte[64];
+    private static final String TEST_DATA_HOME = "bc.test.data.home";
 
-    private static final byte[] K256A = new byte[32];
-    private static final byte[] K256B = Hex
-        .decode("55000000000000000000000000000000"
-            + "00000000000000000000000000000000");
-    private static final byte[] K256FF = Hex
-        .decode("55000000000000000000000000000000"
-            + "000000000000000000000000000000ff");
+    private static class PeekableLineReader extends BufferedReader
+    {
+        public PeekableLineReader(Reader r) throws IOException
+        {
+            super(r);
 
-    private static final byte[] IVA = new byte[32];
-    private static final byte[] IVB = new byte[]{0x1};
-    private static final byte[] IVFF = new byte[]{(byte)0xff};
+            peek = super.readLine();
+        }
 
-    private static final byte[] HC256A = Hex
-        .decode("8589075b0df3f6d82fc0c5425179b6a6"
-            + "3465f053f2891f808b24744e18480b72"
-            + "ec2792cdbf4dcfeb7769bf8dfa14aee4"
-            + "7b4c50e8eaf3a9c8f506016c81697e32");
-    private static final byte[] HC256B = Hex
-        .decode("bfa2e2afe9ce174f8b05c2feb18bb1d1"
-            + "ee42c05f01312b71c61f50dd502a080b"
-            + "edfec706633d9241a6dac448af8561ff"
-            + "5e04135a9448c4342de7e9f337520bdf");
-    private static final byte[] HC256C = Hex
-        .decode("fe4a401ced5fe24fd19a8f956fc036ae"
-            + "3c5aa68823e2abc02f90b3aea8d30e42"
-            + "59f03a6c6e39eb448f7579fb70137a5e"
-            + "6d10b7d8add0f7cd723423daf575dde6");
-    private static final byte[] HC256D = Hex
-        .decode("c6b6fb99f2ae1440a7d4ca342011694e"
-            + "6f36b4be420db05d4745fd907c630695"
-            + "5f1d7bda13ae7e36aebc5399733b7f37"
-            + "95f34066b601d21f2d8cf830a9c08937");
+        public String peekLine()
+        {
+            return peek;
+        }
 
-    private static final byte[] HC256IVFF = Hex
-           .decode("b1b9badd494fa2215bf94b89d6cd5d" +
-               "ae9109d33fbae2ad370641126efb6b" +
-               "d98c8f14298d4bef48fc295c79ac32" +
-               "fd7ea56b0e2a9452d0dc547b0af5d3bfbb598e");
+        public String readLine() throws IOException
+        {
+            String tmp = peek;
+            peek = super.readLine();
+            return tmp;
+        }
 
-    private static final byte[] HC256KFF = Hex
-              .decode("caa5a85b69580d26e49789c62187" +
-                  "a5e6e99f4eba0aa09c2ed8829091" +
-                  "737b18b0510d415bdc008173feff" +
-                  "92818a7d743befe33a85f21782e2" +
-                  "0034d3b334f178e2");
+        private String peek; 
+    }
 
-    private static final byte[] K128A = new byte[16];
-    private static final byte[] K128B = Hex
-        .decode("55000000000000000000000000000000");
-    private static final byte[] K128FF = Hex
-        .decode("550000000000000000000000000000ff");
-
-    private static final byte[] HC128A = Hex
-        .decode("731500823bfd03a0fb2fd77faa63af0e"
-            + "de122fc6a7dc29b662a685278b75ec68"
-            + "9036db1e8189600500ade078491fbf9a"
-            + "1cdc30136c3d6e2490f664b29cd57102");
-    private static final byte[] HC128B = Hex
-        .decode("c01893d5b7dbe9588f65ec9864176604"
-            + "36fc6724c82c6eec1b1c38a7c9b42a95"
-            + "323ef1230a6a908bce757b689f14f7bb"
-            + "e4cde011aeb5173f89608c94b5cf46ca");
-    private static final byte[] HC128C = Hex
-        .decode("518251a404b4930ab02af9310639f032"
-            + "bcb4a47a5722480b2bf99f72cdc0e566"
-            + "310f0c56d3cc83e8663db8ef62dfe07f"
-            + "593e1790c5ceaa9cab03806fc9a6e5a0");
-    private static final byte[] HC128D = Hex
-        .decode("a4eac0267e4911266a2a384f5c4e1329"
-            + "da407fa155e6b1ae05c6fdf3bbdc8a86"
-            + "7a699aa01a4dc11763658cccd3e62474"
-            + "9cf8236f0131be21c3a51de9d12290de");
-
-    private static final byte[] HC128IVFF = Hex
-        .decode("5ae9112eddabe889ca80ea17b852bd30"
-              + "b07ea9e9e9f474e6c5ffe9c1941d1818" +
-                "7424b9813592d22f2a61a520fdf65a2c" +
-                "978baf1eddae6a9fab905990b85d3f46");
-    private static final byte[] HC128KFF = Hex
-        .decode("88913a1d829941005839260b2a877a3a" +
-                "e2b61b93e0c84c5d7968ce0033f85dc6" +
-                "61f6fefb266b19547e25b4648b30a2d4" +
-                "af05b9689543cab3c6ef021ab3860cdc");
-    
     public String getName()
     {
         return "HC-128 and HC-256";
     }
 
-    public void performTest()
+    public void performTest() throws Exception
     {
-        StreamCipher hc = new HC256Engine();
-        HCTest(hc, "HC-256 - A", K256A, IVA, HC256A);
-        HCTest(hc, "HC-256 - B", K256A, IVB, HC256B);
-        HCTest(hc, "HC-256 - C", K256B, IVA, HC256C);
-        HCTest2(hc, "HC-256 - D", K256A, IVA, HC256D, 0x10000);
-
-        HCTest(hc, "HC-256 - A FF K", K256FF, IVA, HC256KFF);
-        HCTest(hc, "HC-256 - A FF IV", K256A, IVFF, HC256IVFF);
-
-        hc = new HC128Engine();
-        HCTest(hc, "HC-128 - A", K128A, IVA, HC128A);
-        HCTest(hc, "HC-128 - B", K128A, IVB, HC128B);
-        HCTest(hc, "HC-128 - C", K128B, IVA, HC128C);
-        HCTest2(hc, "HC-128 - D", K128A, IVA, HC128D, 0x100000);
-
-        HCTest(hc, "HC-256 - A FF K", K128FF, IVA, HC128KFF);
-        HCTest(hc, "HC-128 - A FF IV", K128A, IVFF, HC128IVFF);
+        runTests(new HC128Engine(), "hc-128/ecrypt_HC-128.txt");
+        runTests(new HC256Engine(), "hc-256/ecrypt_HC-256_128K_128IV.txt");
+        runTests(new HC256Engine(), "hc-256/ecrypt_HC-256_256K_128IV.txt");
+        runTests(new HC256Engine(), "hc-256/ecrypt_HC-256_128K_256IV.txt");
+        runTests(new HC256Engine(), "hc-256/ecrypt_HC-256_256K_256IV.txt");
     }
 
-    private void HCTest(StreamCipher hc, String test, byte[] key, byte[] IV, byte[] expected)
+    private void runTests(StreamCipher hc, String fileName) throws IOException
     {
-        KeyParameter kp = new KeyParameter(key);
-        ParametersWithIV ivp = new ParametersWithIV(kp, IV);
+        PeekableLineReader r = new PeekableLineReader(new FileReader(getDataHome() + "/" + fileName));
+        runAllVectors(hc, fileName, r);
+    }
 
-        hc.init(true, ivp);
-        for (int i = 0; i < 64; i++)
+    private void runAllVectors(StreamCipher hc, String fileName, PeekableLineReader r)
+        throws IOException
+    {
+        for (;;)
         {
-            if (hc.returnByte(MSG[i]) != expected[i])
+            String line = r.readLine();
+            if (line == null)
+                break;
+
+            line = line.trim();
+
+            if (line.startsWith("Set "))
             {
-                fail(test + " failure at byte " + i);
+                runVector(hc, fileName, r, line);
             }
         }
     }
 
-    private void HCTest2(StreamCipher hc, String test, byte[] key, byte[] IV, byte[] expected,
-                         int times)
+    private void runVector(StreamCipher hc, String fileName, PeekableLineReader r, String vectorName)
+        throws IOException
     {
-        KeyParameter kp = new KeyParameter(key);
-        ParametersWithIV ivp = new ParametersWithIV(kp, IV);
-        hc.init(true, ivp);
-        byte[] result = new byte[64];
-        for (int j = 0; j < times; j++)
+//        System.out.println(fileName + " => " + vectorName);
+        String hexKey = readBlock(r);
+        String hexIV = readBlock(r);
+
+        CipherParameters cp = new KeyParameter(Hex.decode(hexKey));
+        cp = new ParametersWithIV(cp, Hex.decode(hexIV));
+        hc.init(true, cp);
+
+        byte[] input = new byte[64];
+        byte[] output = new byte[64];
+        byte[] digest = new byte[64];
+        int pos = 0;
+
+        for (;;)
         {
-            for (int i = 0; i < 64; i++)
+            String line1 = r.peekLine().trim();
+            int equalsPos = line1.indexOf('=');
+            String lead = line1.substring(0, equalsPos - 1);
+
+            String hexData = readBlock(r);
+            byte[] data = Hex.decode(hexData);
+
+            if (lead.equals("xor-digest"))
             {
-                result[i] = hc.returnByte(result[i]);
+                if (!Arrays.areEqual(data, digest))
+                {
+                    fail("Failed in " + fileName + " for test vector: " + vectorName + " at " + lead);
+//                  System.out.println(fileName + " => " + vectorName + " failed at " + lead); return;
+                }
+                break;
             }
+
+            int posA = lead.indexOf('[');
+            int posB = lead.indexOf("..");
+            int posC = lead.indexOf(']');
+            int start = Integer.parseInt(lead.substring(posA + 1, posB));
+            int end = Integer.parseInt(lead.substring(posB + 2, posC));
+
+            if (start % 64 != 0 || (end - start != 63))
+                throw new IllegalStateException(vectorName + ": " + lead + " not on 64 byte boundaries");
+
+            while (pos < end)
+            {
+                hc.processBytes(input, 0, input.length, output, 0);
+                xor(digest, output);
+                pos += 64;
+            }
+
+            if (!Arrays.areEqual(data, output))
+            {
+                fail("Failed in " + fileName + " for test vector: " + vectorName + " at " + lead);
+//              System.out.println(fileName + " => " + vectorName + " failed at " + lead); return;
+            }
+        }
+    }
+
+    private static String readBlock(PeekableLineReader r) throws IOException
+    {
+        String first = r.readLine().trim();
+        String result = first.substring(first.lastIndexOf(' ') + 1);
+
+        for (;;)
+        {
+            String peek = r.peekLine().trim();
+            if (peek.length() < 1 || peek.indexOf('=') >= 0)
+            {
+                break;
+            }
+            result += r.readLine().trim();
         }
 
-        for (int i = 0; i < 64; i++)
+        return result;
+    }
+
+    private static void xor(byte[] digest, byte[] block)
+    {
+        for (int i = 0; i < digest.length; ++i)
         {
-            if (result[i] != expected[i])
-            {
-                fail(test + " failure at byte " + i);
-            }
+            digest[i] ^= block[i];
         }
+    }
+
+    private static String getDataHome()
+    {
+        String dataHome = System.getProperty(TEST_DATA_HOME);
+
+        if (dataHome == null)
+        {
+            throw new IllegalStateException(TEST_DATA_HOME + " property not set");
+        }
+
+        return dataHome + "/hc-256";
     }
 
     public static void main(String[] args)
