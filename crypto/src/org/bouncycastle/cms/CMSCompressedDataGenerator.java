fe05bbc88a1f86bd69dc71915348d9e561a11c9c
bouncy <bouncy>
2009-09-07 18:21:00 +0000

    Refactoring

--- crypto/src/org/bouncycastle/cms/CMSCompressedDataGenerator.java
+++ crypto/src/org/bouncycastle/cms/CMSCompressedDataGenerator.java
@@ -1,14 +1,11 @@
 package org.bouncycastle.cms;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.zip.DeflaterOutputStream;
 
-import org.bouncycastle.asn1.ASN1InputStream;
 import org.bouncycastle.asn1.ASN1OctetString;
 import org.bouncycastle.asn1.BERConstructedOctetString;
-import org.bouncycastle.asn1.DERObject;
 import org.bouncycastle.asn1.DERObjectIdentifier;
 import org.bouncycastle.asn1.cms.CMSObjectIdentifiers;
 import org.bouncycastle.asn1.cms.CompressedData;
@@ -37,37 +34,6 @@ public class CMSCompressedDataGenerator
     {
     }
 
-    private DERObject makeObj(
-        byte[]  encoding)
-        throws IOException
-    {
-        if (encoding == null)
-        {
-            return null;
-        }
-
-        ByteArrayInputStream    bIn = new ByteArrayInputStream(encoding);
-        ASN1InputStream         aIn = new ASN1InputStream(bIn);
-
-        return aIn.readObject();
-    }
-
-    private AlgorithmIdentifier makeAlgId(
-        String  oid,
-        byte[]  params)
-        throws IOException
-    {
-        if (params != null)
-        {
-            return new AlgorithmIdentifier(
-                            new DERObjectIdentifier(oid), makeObj(params));
-        }
-        else
-        {
-            return new AlgorithmIdentifier(new DERObjectIdentifier(oid));
-        }
-    }
-
     /**
      * generate an object that contains an CMS Compressed Data
      */
@@ -88,7 +54,7 @@ public class CMSCompressedDataGenerator
 
             zOut.close();
 
-            comAlgId = makeAlgId(compressionOID, null);
+            comAlgId = new AlgorithmIdentifier(new DERObjectIdentifier(compressionOID));
             comOcts = new BERConstructedOctetString(bOut.toByteArray());
         }
         catch (IOException e)
