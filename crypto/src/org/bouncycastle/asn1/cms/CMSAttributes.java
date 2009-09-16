80af4685a92578bdeba8fe7395afab61023a4cd6
bouncy <bouncy>
2009-09-16 05:01:02 +0000

    added 4.6, 4,7, 5.1, and 5.2 tests

--- crypto/src/org/bouncycastle/asn1/cms/CMSAttributes.java
+++ crypto/src/org/bouncycastle/asn1/cms/CMSAttributes.java
@@ -9,4 +9,5 @@ public interface CMSAttributes
     public static final DERObjectIdentifier  messageDigest = PKCSObjectIdentifiers.pkcs_9_at_messageDigest;
     public static final DERObjectIdentifier  signingTime = PKCSObjectIdentifiers.pkcs_9_at_signingTime;
     public static final DERObjectIdentifier  counterSignature = PKCSObjectIdentifiers.pkcs_9_at_counterSignature;
+    public static final DERObjectIdentifier  contentHint = PKCSObjectIdentifiers.id_aa_contentHint;
 }
