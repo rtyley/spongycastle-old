d5b6f806e995fb692b58ce2d0972c9d63259154b
bouncy <bouncy>
2009-11-11 04:30:21 +0000

    Sundry CHOICE objects missing ASN1Choice marker

--- crypto/src/org/bouncycastle/asn1/cms/RecipientIdentifier.java
+++ crypto/src/org/bouncycastle/asn1/cms/RecipientIdentifier.java
@@ -1,5 +1,6 @@
 package org.bouncycastle.asn1.cms;
 
+import org.bouncycastle.asn1.ASN1Choice;
 import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1OctetString;
 import org.bouncycastle.asn1.ASN1TaggedObject;
@@ -9,6 +10,7 @@ import org.bouncycastle.asn1.DERTaggedObject;
 
 public class RecipientIdentifier
     extends ASN1Encodable
+    implements ASN1Choice
 {
     private DEREncodable id;
     
--- crypto/src/org/bouncycastle/asn1/cms/SignerIdentifier.java
+++ crypto/src/org/bouncycastle/asn1/cms/SignerIdentifier.java
@@ -1,5 +1,6 @@
 package org.bouncycastle.asn1.cms;
 
+import org.bouncycastle.asn1.ASN1Choice;
 import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1OctetString;
 import org.bouncycastle.asn1.ASN1TaggedObject;
@@ -9,6 +10,7 @@ import org.bouncycastle.asn1.DERTaggedObject;
 
 public class SignerIdentifier
     extends ASN1Encodable
+    implements ASN1Choice
 {
     private DEREncodable id;
     
