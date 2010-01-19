eb8d6625b856d2f7c857f7d63ea439162c2aa4ee
bouncy <bouncy>
2010-01-19 05:19:55 +0000

    Ensure the clientSigner is properly init'ed

--- crypto/src/org/bouncycastle/crypto/tls/TlsProtocolHandler.java
+++ crypto/src/org/bouncycastle/crypto/tls/TlsProtocolHandler.java
@@ -1164,6 +1164,8 @@ public class TlsProtocolHandler
             {
                 throw new IllegalArgumentException("'clientPrivateKey' type not supported");
             }
+
+            rs.clientSigner.init(true, clientPrivateKey);
         }
 
         this.verifyer = verifyer;
