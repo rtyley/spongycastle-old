a1699635eea585a9343a049d99346bf5da47c97b
bouncy <bouncy>
2010-02-10 13:38:23 +0000

    Refactor to use raw signatures for signing client certificate message

---
+++ crypto/src/org/bouncycastle/crypto/tls/TlsSigner.java
@@ -0,0 +1,11 @@
+package org.bouncycastle.crypto.tls;
+
+import org.bouncycastle.crypto.CryptoException;
+import org.bouncycastle.crypto.Signer;
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
+
+interface TlsSigner
+{
+    byte[] calculateRawSignature(AsymmetricKeyParameter privateKey, byte[] md5andsha1) throws CryptoException;
+    Signer createSigner();
+}
