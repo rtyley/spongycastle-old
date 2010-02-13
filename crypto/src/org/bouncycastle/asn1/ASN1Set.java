7e4546962a57877cad2a4eb0112457678ae92234
bouncy <bouncy>
2010-02-13 09:18:32 +0000

    Refactor lessThanOrEqual

--- crypto/src/org/bouncycastle/asn1/ASN1Set.java
+++ crypto/src/org/bouncycastle/asn1/ASN1Set.java
@@ -227,44 +227,15 @@ abstract public class ASN1Set
          byte[] a,
          byte[] b)
     {
-         if (a.length <= b.length)
-         {
-             for (int i = 0; i != a.length; i++)
-             {
-                 int    l = a[i] & 0xff;
-                 int    r = b[i] & 0xff;
-                 
-                 if (r > l)
-                 {
-                     return true;
-                 }
-                 else if (l > r)
-                 {
-                     return false;
-                 }
-             }
-
-             return true;
-         }
-         else
-         {
-             for (int i = 0; i != b.length; i++)
-             {
-                 int    l = a[i] & 0xff;
-                 int    r = b[i] & 0xff;
-                 
-                 if (r > l)
-                 {
-                     return true;
-                 }
-                 else if (l > r)
-                 {
-                     return false;
-                 }
-             }
-
-             return false;
-         }
+        int len = Math.min(a.length, b.length);
+        for (int i = 0; i != len; ++i)
+        {
+            if (a[i] != b[i])
+            {
+                return (a[i] & 0xff) < (b[i] & 0xff);
+            }
+        }
+        return len == a.length;
     }
 
     private byte[] getEncoded(
