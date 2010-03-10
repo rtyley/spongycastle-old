9cb5a9abe8fd57f0953bc6607343894b78fb834b
bouncy <bouncy>
2010-03-10 01:43:28 +0000

    removed unnecessary code

--- crypto/src/org/bouncycastle/jce/provider/util/NullDigest.java
+++
@@ -1,8 +0,0 @@
-package org.bouncycastle.jce.provider.util;
-
-/**
- * @deprecated use org.bouncycastle.crypto.digests.NullDigest instead
- */
-public class NullDigest extends org.bouncycastle.crypto.digests.NullDigest
-{
-}
