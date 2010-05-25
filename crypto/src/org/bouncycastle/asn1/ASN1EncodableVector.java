7cd9acda3980ed959c9b970fa23872851762c01a
bouncy <bouncy>
2010-05-25 00:12:27 +0000

    readded deprecated file - looks like it needs to stay a bit longer

--- crypto/src/org/bouncycastle/asn1/ASN1EncodableVector.java
+++ crypto/src/org/bouncycastle/asn1/ASN1EncodableVector.java
@@ -6,6 +6,7 @@ import java.util.Vector;
  * the parent class for this will eventually disappear. Use this one!
  */
 public class ASN1EncodableVector
+    extends DEREncodableVector
 {
     Vector v = new Vector();
 
