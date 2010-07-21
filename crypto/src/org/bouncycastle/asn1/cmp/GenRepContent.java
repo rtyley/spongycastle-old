07523c12e10b8216e8adef3537b41daaba7dade3
bouncy <bouncy>
2010-07-21 11:43:57 +0000

    Add missing constructors

--- crypto/src/org/bouncycastle/asn1/cmp/GenRepContent.java
+++ crypto/src/org/bouncycastle/asn1/cmp/GenRepContent.java
@@ -1,8 +1,10 @@
 package org.bouncycastle.asn1.cmp;
 
 import org.bouncycastle.asn1.ASN1Encodable;
+import org.bouncycastle.asn1.ASN1EncodableVector;
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.DERObject;
+import org.bouncycastle.asn1.DERSequence;
 
 public class GenRepContent
     extends ASN1Encodable
@@ -27,6 +29,21 @@ public class GenRepContent
         }
 
         throw new IllegalArgumentException("Invalid object: " + o.getClass().getName());
+    }
+
+    public GenRepContent(InfoTypeAndValue itv)
+    {
+        content = new DERSequence(itv);
+    }
+
+    public GenRepContent(InfoTypeAndValue[] itv)
+    {
+        ASN1EncodableVector v = new ASN1EncodableVector();
+        for (int i = 0; i < itv.length; i++)
+        {
+            v.add(itv[i]);
+        }
+        content = new DERSequence(v);
     }
 
     public InfoTypeAndValue[] toInfoTypeAndValueArray()
