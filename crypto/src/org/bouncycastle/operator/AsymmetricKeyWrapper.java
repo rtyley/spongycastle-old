4f329fd82cf2f30613b1e8885d31825631a545df
bouncy <bouncy>
2010-09-10 08:07:23 +0000

    minor refactoring

--- crypto/src/org/bouncycastle/operator/AsymmetricKeyWrapper.java
+++ crypto/src/org/bouncycastle/operator/AsymmetricKeyWrapper.java
@@ -1,20 +1,19 @@
 package org.bouncycastle.operator;
 
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
-import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 
 public abstract class AsymmetricKeyWrapper
     implements KeyWrapper
 {
-    private SubjectPublicKeyInfo keyInfo;
+    private AlgorithmIdentifier algorithmId;
 
-    protected AsymmetricKeyWrapper(SubjectPublicKeyInfo keyInfo)
+    protected AsymmetricKeyWrapper(AlgorithmIdentifier algorithmId)
     {
-        this.keyInfo = keyInfo;
+        this.algorithmId = algorithmId;
     }
 
     public AlgorithmIdentifier getAlgorithmIdentifier()
     {
-        return keyInfo.getAlgorithmId();
+        return algorithmId;
     }
 }
