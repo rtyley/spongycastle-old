b5068f9303b8078ab310e6b13c39713828453162
bouncy <bouncy>
2010-09-17 04:39:49 +0000

    Introduce ECDSA signer, refactor with DSS

--- crypto/src/org/bouncycastle/crypto/tls/TlsDSSSigner.java
+++ crypto/src/org/bouncycastle/crypto/tls/TlsDSSSigner.java
@@ -1,29 +1,12 @@
 package org.bouncycastle.crypto.tls;
 
-import org.bouncycastle.crypto.CryptoException;
-import org.bouncycastle.crypto.Signer;
-import org.bouncycastle.crypto.digests.NullDigest;
-import org.bouncycastle.crypto.digests.SHA1Digest;
-import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
-import org.bouncycastle.crypto.signers.DSADigestSigner;
+import org.bouncycastle.crypto.DSA;
 import org.bouncycastle.crypto.signers.DSASigner;
 
-class TlsDSSSigner implements TlsSigner
+class TlsDSSSigner extends TlsDSASigner
 {
-    public byte[] calculateRawSignature(AsymmetricKeyParameter privateKey, byte[] md5andsha1)
-        throws CryptoException
+    protected DSA createDSAImpl()
     {
-        // Note: Only use the SHA1 part of the hash
-        Signer sig = new DSADigestSigner(new DSASigner(), new NullDigest());
-        sig.init(true, privateKey);
-        sig.update(md5andsha1, 16, 20);
-        return sig.generateSignature();
-    }
-
-    public Signer createVerifyer(AsymmetricKeyParameter publicKey)
-    {
-        Signer s = new DSADigestSigner(new DSASigner(), new SHA1Digest());
-        s.init(false, publicKey);
-        return s;
+        return new DSASigner();
     }
 }
