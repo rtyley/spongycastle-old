e540b76ed879781ba164300d8d107a12bc9103cc
bouncy <bouncy>
2010-09-19 19:05:21 +0000

    Refactor to avoid a few unnecessary conversions, and to make it clearer how explicit params will work

--- crypto/src/org/bouncycastle/crypto/tls/TlsECDHEKeyExchange.java
+++ crypto/src/org/bouncycastle/crypto/tls/TlsECDHEKeyExchange.java
@@ -4,14 +4,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import org.bouncycastle.asn1.ASN1OctetString;
-import org.bouncycastle.asn1.DEROctetString;
-import org.bouncycastle.asn1.x9.X9ECPoint;
 import org.bouncycastle.crypto.Signer;
 import org.bouncycastle.crypto.io.SignerInputStream;
 import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
 import org.bouncycastle.crypto.params.ECDomainParameters;
 import org.bouncycastle.crypto.params.ECPublicKeyParameters;
+import org.bouncycastle.math.ec.ECPoint;
 
 /**
  * ECDHE key exchange (see RFC 4492)
@@ -54,37 +52,47 @@ class TlsECDHEKeyExchange extends TlsECKeyExchange
         }
 
         short curveType = TlsUtils.readUint8(sigIn);
+        ECDomainParameters curve_params;
 
-        // Currently, we only support named curves
+        //  Currently, we only support named curves
         if (curveType == ECCurveType.named_curve)
         {
             int namedCurve = TlsUtils.readUint16(sigIn);
-            short ephemeralKeyLength = TlsUtils.readUint8(sigIn);
-            byte[] ephemeralKey = new byte[ephemeralKeyLength];
-            TlsUtils.readFully(ephemeralKey, sigIn);
-            if (signer != null)
-            {
-                byte[] sigByte = TlsUtils.readOpaque16(is);
 
-                if (!signer.verifySignature(sigByte))
-                {
-                    handler.failWithError(AlertLevel.fatal, AlertDescription.bad_certificate);
-                }
-            }
-            serverEphemeralPublicKey = parsePublicKey(ephemeralKey, namedCurve);
-            clientEphemeralKeyPair = generateECKeyPair(serverEphemeralPublicKey.getParameters());
+            // TODO Check namedCurve is one we offered?
 
+            curve_params = NamedCurve.getECParameters(namedCurve);
         }
         else
         {
-            // TODO Add support for explicit curve parameters
+            // TODO Add support for explicit curve parameters (read from sigIn)
 
             handler.failWithError(AlertLevel.fatal, AlertDescription.handshake_failure);
+            return;
         }
+
+        byte[] publicBytes = TlsUtils.readOpaque8(sigIn);
+
+        if (signer != null)
+        {
+            byte[] sigByte = TlsUtils.readOpaque16(is);
+
+            if (!signer.verifySignature(sigByte))
+            {
+                handler.failWithError(AlertLevel.fatal, AlertDescription.bad_certificate);
+            }
+        }
+
+        // TODO Check curve_params not null
+
+        ECPoint Q = curve_params.getCurve().decodePoint(publicBytes);
+
+        serverEphemeralPublicKey = new ECPublicKeyParameters(Q, curve_params);
     }
 
     public void generateClientKeyExchange(OutputStream os) throws IOException
     {
+        clientEphemeralKeyPair = generateECKeyPair(serverEphemeralPublicKey.getParameters());
         byte[] keData = externalizeKey((ECPublicKeyParameters)clientEphemeralKeyPair.getPublic());
         TlsUtils.writeUint24(keData.length + 1, os);
         TlsUtils.writeOpaque8(keData, os);
@@ -94,15 +102,6 @@ class TlsECDHEKeyExchange extends TlsECKeyExchange
     {
         return calculateECDHEPreMasterSecret((ECPublicKeyParameters)serverEphemeralPublicKey,
             clientEphemeralKeyPair.getPrivate());
-    }
-
-    ECPublicKeyParameters parsePublicKey(byte[] bytes, int namedCurve) throws IOException
-    {
-        ECDomainParameters dParams = null;
-        dParams = NamedCurve.getECParameters(namedCurve);
-        ASN1OctetString key = new DEROctetString(bytes);
-        X9ECPoint derQ = new X9ECPoint(dParams.getCurve(), key);
-        return new ECPublicKeyParameters(derQ.getPoint(), dParams);
     }
 
 //    public void processServerCertificateRequest(byte[] certificateTypes,
