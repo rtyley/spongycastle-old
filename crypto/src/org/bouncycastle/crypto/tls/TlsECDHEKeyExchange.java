ac50b7b9dfe1507a3ca51228f1820a7d3ebe4079
bouncy <bouncy>
2010-09-19 14:48:07 +0000

    Add basic implementations of EC key exchange (minus client auth atm)

---
+++ crypto/src/org/bouncycastle/crypto/tls/TlsECDHEKeyExchange.java
@@ -0,0 +1,120 @@
+package org.bouncycastle.crypto.tls;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.bouncycastle.asn1.ASN1OctetString;
+import org.bouncycastle.asn1.DEROctetString;
+import org.bouncycastle.asn1.x9.X9ECPoint;
+import org.bouncycastle.crypto.Signer;
+import org.bouncycastle.crypto.io.SignerInputStream;
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
+import org.bouncycastle.crypto.params.ECDomainParameters;
+import org.bouncycastle.crypto.params.ECPublicKeyParameters;
+
+/**
+ * ECDHE key exchange (see RFC 4492)
+ */
+class TlsECDHEKeyExchange extends TlsECKeyExchange
+{
+    TlsECDHEKeyExchange(TlsProtocolHandler handler, CertificateVerifyer verifyer,
+        short keyExchange,
+        // TODO Replace with an interface e.g. TlsClientAuth
+        Certificate clientCert, AsymmetricKeyParameter clientPrivateKey)
+    {
+        super(handler, verifyer, keyExchange, clientCert, clientPrivateKey);
+    }
+
+    public void skipServerCertificate() throws IOException
+    {
+        handler.failWithError(TlsProtocolHandler.AL_fatal, TlsProtocolHandler.AP_unexpected_message);
+    }
+
+    public void skipServerKeyExchange() throws IOException
+    {
+        handler.failWithError(TlsProtocolHandler.AL_fatal, TlsProtocolHandler.AP_unexpected_message);
+    }
+
+    public void processServerKeyExchange(InputStream is, SecurityParameters securityParameters)
+        throws IOException
+    {
+        if (tlsSigner == null)
+        {
+            handler.failWithError(TlsProtocolHandler.AL_fatal,
+                TlsProtocolHandler.AP_unexpected_message);
+        }
+
+        InputStream sigIn = is;
+        Signer signer = null;
+
+        if (tlsSigner != null)
+        {
+            signer = initSigner(tlsSigner, securityParameters);
+            sigIn = new SignerInputStream(is, signer);
+        }
+
+        short curveType = TlsUtils.readUint8(sigIn);
+
+        // Currently, we only support named curves
+        if (curveType == ECCurveType.named_curve)
+        {
+            int namedCurve = TlsUtils.readUint16(sigIn);
+            short ephemeralKeyLength = TlsUtils.readUint8(sigIn);
+            byte[] ephemeralKey = new byte[ephemeralKeyLength];
+            TlsUtils.readFully(ephemeralKey, sigIn);
+            if (signer != null)
+            {
+                byte[] sigByte = TlsUtils.readOpaque16(is);
+
+                if (!signer.verifySignature(sigByte))
+                {
+                    handler.failWithError(TlsProtocolHandler.AL_fatal,
+                        TlsProtocolHandler.AP_bad_certificate);
+                }
+            }
+            serverEphemeralPublicKey = parsePublicKey(ephemeralKey, namedCurve);
+            clientEphemeralKeyPair = generateECKeyPair(serverEphemeralPublicKey.getParameters());
+
+        }
+        else
+        {
+            // TODO Add support for explicit curve parameters
+
+            handler.failWithError(TlsProtocolHandler.AL_fatal,
+                TlsProtocolHandler.AP_handshake_failure);
+        }
+    }
+
+    public void generateClientKeyExchange(OutputStream os) throws IOException
+    {
+        byte[] keData = externalizeKey((ECPublicKeyParameters)clientEphemeralKeyPair.getPublic());
+        TlsUtils.writeUint24(keData.length + 1, os);
+        TlsUtils.writeOpaque8(keData, os);
+    }
+
+    public byte[] generatePremasterSecret() throws IOException
+    {
+        return calculateECDHEPreMasterSecret((ECPublicKeyParameters)serverEphemeralPublicKey,
+            clientEphemeralKeyPair.getPrivate());
+    }
+
+    ECPublicKeyParameters parsePublicKey(byte[] bytes, int namedCurve) throws IOException
+    {
+        ECDomainParameters dParams = null;
+        dParams = NamedCurve.getECParameters(namedCurve);
+        ASN1OctetString key = new DEROctetString(bytes);
+        X9ECPoint derQ = new X9ECPoint(dParams.getCurve(), key);
+        return new ECPublicKeyParameters(derQ.getPoint(), dParams);
+    }
+
+//    public void processServerCertificateRequest(byte[] certificateTypes,
+//        Vector certificateAuthorities)
+//    {
+//    }
+//
+//    public boolean sendCertificateVerify()
+//    {
+//        return true;
+//    }
+}
---
+++ crypto/src/org/bouncycastle/crypto/tls/TlsECKeyExchange.java
@@ -0,0 +1,213 @@
+package org.bouncycastle.crypto.tls;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+import org.bouncycastle.asn1.DERBitString;
+import org.bouncycastle.asn1.x509.KeyUsage;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.asn1.x509.X509CertificateStructure;
+import org.bouncycastle.asn1.x509.X509Extension;
+import org.bouncycastle.asn1.x509.X509Extensions;
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.crypto.CipherParameters;
+import org.bouncycastle.crypto.Signer;
+import org.bouncycastle.crypto.agreement.ECDHBasicAgreement;
+import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
+import org.bouncycastle.crypto.params.ECDomainParameters;
+import org.bouncycastle.crypto.params.ECKeyGenerationParameters;
+import org.bouncycastle.crypto.params.ECPublicKeyParameters;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.bouncycastle.crypto.util.PublicKeyFactory;
+import org.bouncycastle.math.ec.ECPoint;
+import org.bouncycastle.util.BigIntegers;
+
+/**
+ * Base class for EC key exchange algorithms (see RFC 4492)
+ */
+abstract class TlsECKeyExchange implements TlsKeyExchange
+{
+
+    protected TlsProtocolHandler handler;
+    protected CertificateVerifyer verifyer;
+    protected short keyExchange;
+    protected TlsSigner tlsSigner;
+
+    protected AsymmetricKeyParameter serverPublicKey;
+
+    protected  AsymmetricCipherKeyPair clientEphemeralKeyPair;
+    protected ECPublicKeyParameters serverEphemeralPublicKey;
+    
+    protected Certificate clientCert;
+    protected AsymmetricKeyParameter clientPrivateKey = null;
+
+    TlsECKeyExchange(TlsProtocolHandler handler, CertificateVerifyer verifyer, short keyExchange,
+        // TODO Replace with an interface e.g. TlsClientAuth
+        Certificate clientCert, AsymmetricKeyParameter clientPrivateKey)
+    {
+        switch (keyExchange)
+        {
+            case KE_ECDH_RSA:
+            case KE_ECDHE_RSA:
+                this.tlsSigner = new TlsRSASigner();
+                break;
+            case KE_ECDH_ECDSA:
+            case KE_ECDHE_ECDSA: 
+                this.tlsSigner = new TlsECDSASigner();
+                break;
+            case KE_ECDH_anon:
+                this.tlsSigner = null;
+                break;
+            default:
+                throw new IllegalArgumentException("unsupported key exchange algorithm");
+        }
+
+        this.handler = handler;
+        this.verifyer = verifyer;
+        this.keyExchange = keyExchange;
+        this.clientCert = clientCert;
+        this.clientPrivateKey = clientPrivateKey;
+    }
+
+    public void skipServerCertificate() throws IOException
+    {
+        handler.failWithError(TlsProtocolHandler.AL_fatal, TlsProtocolHandler.AP_unexpected_message);
+    }
+
+    public void processServerCertificate(Certificate serverCertificate) throws IOException
+    {
+        X509CertificateStructure x509Cert = serverCertificate.certs[0];
+        SubjectPublicKeyInfo keyInfo = x509Cert.getSubjectPublicKeyInfo();
+
+        try
+        {
+            this.serverPublicKey = PublicKeyFactory.createKey(keyInfo);
+        }
+        catch (RuntimeException e)
+        {
+            handler.failWithError(TlsProtocolHandler.AL_fatal,
+                TlsProtocolHandler.AP_unsupported_certificate);
+        }
+
+        // Sanity check the PublicKeyFactory
+        if (this.serverPublicKey.isPrivate())
+        {
+            handler.failWithError(TlsProtocolHandler.AL_fatal, TlsProtocolHandler.AP_internal_error);
+        }
+
+        // TODO 
+        /*
+         * Perform various checks per RFC2246 7.4.2: "Unless otherwise specified, the
+         * signing algorithm for the certificate must be the same as the algorithm for the
+         * certificate key."
+         */
+
+        // TODO Should the 'instanceof' tests be replaces with stricter checks on keyInfo.getAlgorithmId()?
+
+        switch (this.keyExchange)
+        {    
+            case KE_ECDH_ECDSA:
+                if (!(this.serverPublicKey instanceof ECPublicKeyParameters))
+                {
+                    handler.failWithError(TlsProtocolHandler.AL_fatal,
+                        TlsProtocolHandler.AP_certificate_unknown);
+                }
+                // TODO The algorithm used to sign the certificate should be DSS.
+//                x509Cert.getSignatureAlgorithm();
+                break;
+            case KE_ECDHE_ECDSA:
+                if (!(this.serverPublicKey instanceof ECPublicKeyParameters))
+                {
+                    handler.failWithError(TlsProtocolHandler.AL_fatal,
+                        TlsProtocolHandler.AP_certificate_unknown);
+                }
+                // TODO The algorithm used to sign the certificate should be RSA.
+//              x509Cert.getSignatureAlgorithm();
+                break;
+            case KE_ECDH_RSA:
+                if (!(this.serverPublicKey instanceof RSAKeyParameters))
+                {
+                    handler.failWithError(TlsProtocolHandler.AL_fatal,
+                        TlsProtocolHandler.AP_certificate_unknown);
+                }
+                validateKeyUsage(x509Cert, KeyUsage.digitalSignature);
+                break;
+            case KE_ECDHE_RSA:
+                if (!(this.serverPublicKey instanceof RSAKeyParameters))
+                {
+                    handler.failWithError(TlsProtocolHandler.AL_fatal,
+                        TlsProtocolHandler.AP_certificate_unknown);
+                }
+                break;
+
+            case KE_ECDH_anon:
+                // todo 
+                break;
+            default:
+                handler.failWithError(TlsProtocolHandler.AL_fatal,
+                    TlsProtocolHandler.AP_unsupported_certificate);
+        }
+
+        /*
+         * Verify them.
+         */
+        if (!this.verifyer.isValid(serverCertificate.getCerts()))
+        {
+            handler.failWithError(TlsProtocolHandler.AL_fatal, TlsProtocolHandler.AP_user_canceled);
+        }
+    }
+
+    AsymmetricCipherKeyPair generateECKeyPair(ECDomainParameters parameters)
+    {
+        ECKeyPairGenerator keyPairGenerator = new ECKeyPairGenerator();
+        ECKeyGenerationParameters keyGenerationParameters = new ECKeyGenerationParameters(
+            parameters, handler.getRandom());
+
+        keyPairGenerator.init(keyGenerationParameters);
+        AsymmetricCipherKeyPair keyPair = keyPairGenerator.generateKeyPair();
+        return keyPair;
+    }
+
+    byte[] externalizeKey(ECPublicKeyParameters keyParameters) throws IOException
+    {
+          ECPoint ecPoint = keyParameters.getQ();
+          return ecPoint.getEncoded();
+    }
+
+    byte[] calculateECDHEPreMasterSecret(ECPublicKeyParameters publicKey,
+        CipherParameters privateKey)
+    {
+        ECDHBasicAgreement basicAgreement = new ECDHBasicAgreement();
+        basicAgreement.init(privateKey);
+        BigInteger agreement = basicAgreement.calculateAgreement(publicKey);
+        return BigIntegers.asUnsignedByteArray(agreement);
+    }
+
+    void validateKeyUsage(X509CertificateStructure c, int keyUsageBits) throws IOException
+    {
+        X509Extensions exts = c.getTBSCertificate().getExtensions();
+        if (exts != null)
+        {
+            X509Extension ext = exts.getExtension(X509Extensions.KeyUsage);
+            if (ext != null)
+            {
+                DERBitString ku = KeyUsage.getInstance(ext);
+                int bits = ku.getBytes()[0] & 0xff;
+                if ((bits & keyUsageBits) != keyUsageBits)
+                {
+                    handler.failWithError(TlsProtocolHandler.AL_fatal,
+                        TlsProtocolHandler.AP_certificate_unknown);
+                }
+            }
+        }
+    }
+
+    Signer initSigner(TlsSigner tlsSigner, SecurityParameters securityParameters)
+    {
+        Signer signer = tlsSigner.createVerifyer(this.serverPublicKey);
+        signer.update(securityParameters.clientRandom, 0, securityParameters.clientRandom.length);
+        signer.update(securityParameters.serverRandom, 0, securityParameters.serverRandom.length);
+        return signer;
+    }
+}
