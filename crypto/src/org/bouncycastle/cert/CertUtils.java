6bb90e97e83a553cd4df547ed59489aa75bcac8f
bouncy <bouncy>
2010-11-29 22:49:35 +0000

    added extension handling to holder classes.

--- crypto/src/org/bouncycastle/cert/CertUtils.java
+++ crypto/src/org/bouncycastle/cert/CertUtils.java
@@ -2,6 +2,12 @@ package org.bouncycastle.cert;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.bouncycastle.asn1.ASN1EncodableVector;
 import org.bouncycastle.asn1.DERBitString;
@@ -9,10 +15,14 @@ import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.asn1.x509.TBSCertificateStructure;
 import org.bouncycastle.asn1.x509.X509CertificateStructure;
+import org.bouncycastle.asn1.x509.X509Extensions;
 import org.bouncycastle.operator.ContentSigner;
 
 class CertUtils
 {
+    private static Set EMPTY_SET = Collections.unmodifiableSet(new HashSet());
+    private static List EMPTY_LIST = Collections.unmodifiableList(new ArrayList());
+
     static X509CertificateHolder generateFullCert(ContentSigner signer, TBSCertificateStructure tbsCert)
     {
         try
@@ -40,5 +50,36 @@ class CertUtils
         v.add(new DERBitString(signature));
 
         return X509CertificateStructure.getInstance(new DERSequence(v));
+    }
+
+    static Set getCriticalExtensionOIDs(X509Extensions extensions)
+    {
+        if (extensions == null)
+        {
+            return EMPTY_SET;
+        }
+
+        return Collections.unmodifiableSet(new HashSet(Arrays.asList(extensions.getCriticalExtensionOIDs())));
+    }
+
+    static Set getNonCriticalExtensionOIDs(X509Extensions extensions)
+    {
+        if (extensions == null)
+        {
+            return EMPTY_SET;
+        }
+
+        // TODO: should probably produce a set that imposes correct ordering
+        return Collections.unmodifiableSet(new HashSet(Arrays.asList(extensions.getNonCriticalExtensionOIDs())));
+    }
+
+    static List getExtensionOIDs(X509Extensions extensions)
+    {
+        if (extensions == null)
+        {
+            return EMPTY_LIST;
+        }
+
+        return Collections.unmodifiableList(Arrays.asList(extensions.getExtensionOIDs()));
     }
 }
--- crypto/src/org/bouncycastle/cert/X509AttributeCertificateHolder.java
+++ crypto/src/org/bouncycastle/cert/X509AttributeCertificateHolder.java
@@ -2,11 +2,17 @@ package org.bouncycastle.cert;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.x509.AttCertValidityPeriod;
 import org.bouncycastle.asn1.x509.AttributeCertificate;
 import org.bouncycastle.asn1.x509.AttributeCertificateInfo;
 import org.bouncycastle.asn1.x509.X509Extension;
+import org.bouncycastle.asn1.x509.X509Extensions;
 import org.bouncycastle.operator.ContentVerifier;
 import org.bouncycastle.operator.ContentVerifierProvider;
 import org.bouncycastle.util.Arrays;
@@ -14,15 +20,17 @@ import org.bouncycastle.util.Arrays;
 public class X509AttributeCertificateHolder
 {
     private AttributeCertificate attrCert;
+    private X509Extensions extensions;
 
     public X509AttributeCertificateHolder(byte[] certEncoding)
     {
-        this.attrCert = AttributeCertificate.getInstance(certEncoding);
+        this(AttributeCertificate.getInstance(certEncoding));
     }
 
     public X509AttributeCertificateHolder(AttributeCertificate attrCert)
     {
         this.attrCert = attrCert;
+        this.extensions = attrCert.getAcinfo().getExtensions();
     }
 
     public byte[] getEncoded()
@@ -31,14 +39,53 @@ public class X509AttributeCertificateHolder
         return attrCert.getEncoded();
     }
 
+    public boolean hasExtensions()
+    {
+        return extensions != null;
+    }
+
     public X509Extension getExtension(ASN1ObjectIdentifier oid)
     {
-        return attrCert.getAcinfo().getExtensions().getExtension(oid);
+        if (extensions != null)
+        {
+            return extensions.getExtension(oid);
+        }
+
+        return null;
+    }
+
+    public List getExtensionOIDs()
+    {
+        return CertUtils.getExtensionOIDs(extensions);
+    }
+
+    public Set getCriticalExtensionOIDs()
+    {
+        return CertUtils.getCriticalExtensionOIDs(extensions);
+    }
+
+    public Set getNonCriticalExtensionOIDs()
+    {
+        return CertUtils.getNonCriticalExtensionOIDs(extensions);
     }
 
     public AttributeCertificate toASN1Structure()
     {
         return attrCert;
+    }
+
+    public boolean isValidOn(Date date)
+    {
+        AttCertValidityPeriod certValidityPeriod = attrCert.getAcinfo().getAttrCertValidityPeriod();
+
+        try
+        {
+            return !date.before(certValidityPeriod.getNotBeforeTime().getDate()) && !date.after(certValidityPeriod.getNotAfterTime().getDate());
+        }
+        catch (ParseException e)
+        {
+            throw new IllegalStateException("unable to parse dates in attribute certificate");
+        }
     }
 
     public boolean isSignatureValid(ContentVerifierProvider verifierProvider)
