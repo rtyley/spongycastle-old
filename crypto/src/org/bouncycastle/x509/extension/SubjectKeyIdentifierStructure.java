02253efef686ec8b207eae2d03f1edec55311178
bouncy <bouncy>
2010-12-14 01:47:00 +0000

    BJA-237 fixed crazy exception throwing.

--- crypto/src/org/bouncycastle/x509/extension/SubjectKeyIdentifierStructure.java
+++ crypto/src/org/bouncycastle/x509/extension/SubjectKeyIdentifierStructure.java
@@ -1,8 +1,8 @@
 package org.bouncycastle.x509.extension;
 
 import java.io.IOException;
+import java.security.InvalidKeyException;
 import java.security.PublicKey;
-import java.security.cert.CertificateParsingException;
 
 import org.bouncycastle.asn1.ASN1OctetString;
 import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
@@ -29,7 +29,7 @@ public class SubjectKeyIdentifierStructure
     
     private static ASN1OctetString fromPublicKey(
         PublicKey pubKey)
-        throws CertificateParsingException
+        throws InvalidKeyException
     {
         try
         {
@@ -39,13 +39,13 @@ public class SubjectKeyIdentifierStructure
         }
         catch (Exception e)
         {
-            throw new CertificateParsingException("Exception extracting certificate details: " + e.toString());
+            throw new InvalidKeyException("Exception extracting key details: " + e.toString());
         }
     }
     
     public SubjectKeyIdentifierStructure(
         PublicKey pubKey)
-        throws CertificateParsingException
+        throws InvalidKeyException
     {
         super(fromPublicKey(pubKey));
     }
