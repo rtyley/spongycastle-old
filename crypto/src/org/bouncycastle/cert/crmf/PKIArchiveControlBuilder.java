3883d01ff9d07bc5f38a98cbd6800e804bf60f5f
bouncy <bouncy>
2010-12-21 00:36:22 +0000

    JavaDoc, minor refactoring.

--- crypto/src/org/bouncycastle/cert/crmf/PKIArchiveControlBuilder.java
+++ crypto/src/org/bouncycastle/cert/crmf/PKIArchiveControlBuilder.java
@@ -16,11 +16,20 @@ import org.bouncycastle.cms.CMSProcessableByteArray;
 import org.bouncycastle.cms.RecipientInfoGenerator;
 import org.bouncycastle.operator.OutputEncryptor;
 
+/**
+ * Builder for a PKIArchiveControl structure.
+ */
 public class PKIArchiveControlBuilder
 {
     private CMSEnvelopedDataGenerator envGen;
     private CMSProcessableByteArray keyContent;
 
+    /**
+     * Basic constructor - specify the contents of the PKIArchiveControl structure.
+     *
+     * @param privateKeyInfo the private key to be archived.
+     * @param generalName the general name to be associated with the private key.
+     */
     public PKIArchiveControlBuilder(PrivateKeyInfo privateKeyInfo, GeneralName generalName)
     {
         EncKeyWithID encKeyWithID = new EncKeyWithID(privateKeyInfo, generalName);
@@ -37,6 +46,12 @@ public class PKIArchiveControlBuilder
         this.envGen = new CMSEnvelopedDataGenerator();
     }
 
+    /**
+     * Add a recipient generator to this control.
+     *
+     * @param recipientGen recipient generator created for a specific recipient.
+     * @return this builder object.
+     */
     public PKIArchiveControlBuilder addRecipientGenerator(RecipientInfoGenerator recipientGen)
     {
         envGen.addRecipientInfoGenerator(recipientGen);
@@ -44,6 +59,13 @@ public class PKIArchiveControlBuilder
         return this;
     }
 
+    /**
+     * Build the PKIArchiveControl using the passed in encryptor to encrypt its contents.
+     *
+     * @param contentEncryptor a suitable content encryptor.
+     * @return a PKIArchiveControl object.
+     * @throws CMSException in the event the build fails.
+     */
     public PKIArchiveControl build(OutputEncryptor contentEncryptor)
         throws CMSException
     {
--- crypto/src/org/bouncycastle/cert/crmf/RegTokenControl.java
+++ crypto/src/org/bouncycastle/cert/crmf/RegTokenControl.java
@@ -5,6 +5,9 @@ import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.DERUTF8String;
 import org.bouncycastle.asn1.crmf.CRMFObjectIdentifiers;
 
+/**
+ * Carrier for a registration token control.
+ */
 public class RegTokenControl
     implements Control
 {
@@ -12,21 +15,41 @@ public class RegTokenControl
 
     private final DERUTF8String token;
 
+    /**
+     * Basic constructor - build from a UTF-8 string representing the token.
+     *
+     * @param token UTF-8 string representing the token.
+     */
     public RegTokenControl(DERUTF8String token)
     {
         this.token = token;
     }
 
+    /**
+     * Basic constructor - build from a string representing the token.
+     *
+     * @param token string representing the token.
+     */
     public RegTokenControl(String token)
     {
         this.token = new DERUTF8String(token);
     }
 
+    /**
+     * Return the type of this control.
+     *
+     * @return CRMFObjectIdentifiers.id_regCtrl_regToken
+     */
     public ASN1ObjectIdentifier getType()
     {
         return type;
     }
 
+    /**
+     * Return the token associated with this control (a UTF8String).
+     *
+     * @return a UTF8String.
+     */
     public ASN1Encodable getValue()
     {
         return token;
