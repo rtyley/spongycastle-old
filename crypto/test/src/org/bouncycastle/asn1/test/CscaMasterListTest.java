7ef6489e81f92ee744225e808da9a03716a74c5e
bouncy <bouncy>
2011-02-07 04:38:43 +0000

    change java.util import to BC one.

--- crypto/test/src/org/bouncycastle/asn1/test/CscaMasterListTest.java
+++ crypto/test/src/org/bouncycastle/asn1/test/CscaMasterListTest.java
@@ -1,34 +1,36 @@
 package org.bouncycastle.asn1.test;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 import org.bouncycastle.asn1.ASN1Object;
 import org.bouncycastle.asn1.icao.CscaMasterList;
+import org.bouncycastle.util.Arrays;
 import org.bouncycastle.util.io.Streams;
 import org.bouncycastle.util.test.SimpleTest;
 
-public class CscaMasterListTest 
+public class CscaMasterListTest
     extends SimpleTest
 {
     public String getName()
     {
         return "CscaMasterList";
     }
-    
-    public void performTest() 
+
+    public void performTest()
         throws Exception
     {
         byte[] input = getInput("masterlist-content.data");
         CscaMasterList parsedList
             = CscaMasterList.getInstance(ASN1Object.fromByteArray(input));
 
-        if (parsedList.getCertStructs().length != 3) {
+        if (parsedList.getCertStructs().length != 3)
+        {
             fail("Cert structure parsing failed: incorrect length");
         }
 
         byte[] output = parsedList.getEncoded();
-        if (!Arrays.equals(input, output)) {
+        if (!Arrays.areEqual(input, output))
+        {
             fail("Encoding failed after parse");
         }
     }
@@ -40,7 +42,7 @@ public class CscaMasterListTest
     }
 
     public static void main(
-        String[]    args)
+        String[] args)
     {
         runTest(new CscaMasterListTest());
     }
