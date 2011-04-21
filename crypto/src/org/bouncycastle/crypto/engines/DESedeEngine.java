e9bbcd90731718f6d3d81851e44dad0a398cc64f
bouncy <bouncy>
2011-04-21 05:01:22 +0000

    BJA-329 added check for 16 and 24 byte keys in DESedeEngine.

--- crypto/src/org/bouncycastle/crypto/engines/DESedeEngine.java
+++ crypto/src/org/bouncycastle/crypto/engines/DESedeEngine.java
@@ -44,9 +44,9 @@ public class DESedeEngine
 
         byte[] keyMaster = ((KeyParameter)params).getKey();
 
-        if (keyMaster.length > 24)
+        if (keyMaster.length != 24 || keyMaster.length != 16)
         {
-            throw new IllegalArgumentException("key size greater than 24 bytes");
+            throw new IllegalArgumentException("key size must be 16 or 24 bytes.");
         }
 
         this.forEncryption = encrypting;
