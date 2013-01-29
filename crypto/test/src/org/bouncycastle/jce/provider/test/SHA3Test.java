a62361a630f46dba1afcab7113a9a5d2b26fe16b
bouncy <bouncy>
2013-01-29 10:57:40 +0000

    fixed typo in algorithm name

--- crypto/test/src/org/bouncycastle/jce/provider/test/SHA3Test.java
+++ crypto/test/src/org/bouncycastle/jce/provider/test/SHA3Test.java
@@ -13,15 +13,15 @@ public class SHA3Test
 
     static private String[][] nullVectors =
     {
-        { "MessageDigest.SHA3-224", "f71837502ba8e10837bdd8d365adb85591895602fc552b48b7390abd" },
-        { "MessageDigest.SHA3-256", "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470" },
-        { "MessageDigest.SHA3-384", "2c23146a63a29acf99e73b88f8c24eaa7dc60aa771780ccc006afbfa8fe2479b2dd2b21362337441ac12b515911957ff" },
-        { "MessageDigest.SHA3-512", "0eab42de4c3ceb9235fc91acffe746b29c29a8c366b7c60e4e67c466f36a4304c00fa9caf9d87976ba469bcbe06713b435f091ef2769fb160cdab33d3670680e" },
+        { "SHA3-224", "f71837502ba8e10837bdd8d365adb85591895602fc552b48b7390abd" },
+        { "SHA3-256", "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470" },
+        { "SHA3-384", "2c23146a63a29acf99e73b88f8c24eaa7dc60aa771780ccc006afbfa8fe2479b2dd2b21362337441ac12b515911957ff" },
+        { "SHA3-512", "0eab42de4c3ceb9235fc91acffe746b29c29a8c366b7c60e4e67c466f36a4304c00fa9caf9d87976ba469bcbe06713b435f091ef2769fb160cdab33d3670680e" },
     };
     
     public String getName()
     {
-        return "MessageDigest.SHA3";
+        return "SHA3";
     }
 
     void test(String algorithm)
