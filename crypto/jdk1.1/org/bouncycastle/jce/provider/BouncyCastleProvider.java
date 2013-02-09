68d6c3ae74741d120375ad2261e1e56658dbf7c8
bouncy <bouncy>
2013-02-09 11:29:50 +0000

    updated provider version number

--- crypto/jdk1.1/org/bouncycastle/jce/provider/BouncyCastleProvider.java
+++ crypto/jdk1.1/org/bouncycastle/jce/provider/BouncyCastleProvider.java
@@ -44,7 +44,7 @@ import org.bouncycastle.jcajce.provider.util.AsymmetricKeyInfoConverter;
 public final class BouncyCastleProvider extends Provider
     implements ConfigurableProvider
 {
-    private static String info = "BouncyCastle Security Provider v1.47";
+    private static String info = "BouncyCastle Security Provider v1.48";
 
     public static String PROVIDER_NAME = "BC";
 
@@ -96,7 +96,7 @@ public final class BouncyCastleProvider extends Provider
      */
     public BouncyCastleProvider()
     {
-        super(PROVIDER_NAME, 1.47, info);
+        super(PROVIDER_NAME, 1.48, info);
 
                 setup();
     }
